776a155c7d150db71704952a19b1fcbdd6d5e543
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-10-18 00:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (89.76%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        vector<int> digits;
+        int temp = num;
+        while (temp != 0) {
+            digits.push_back(temp % 10);
+            temp /= 10;
+        }
+
+        reverse(digits.begin(), digits.end());
+
+        vector<int> max_right(digits.size());
+        int max_idx = digits.size() - 1;
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] > digits[max_idx]) {
+                max_idx = i;
+            }
+            max_right[i] = max_idx;
+        }
+
+        for (int i = 0; i < digits.size(); ++i) {
+            if (digits[i] != digits[max_right[i]]) {
+                swap(digits[i], digits[max_right[i]]);
+                break;
+            }
+        }
+
+        int result = 0;
+        for (int digit : digits) {
+            result = result * 10 + digit;
+        }
+
+        return result;
+    }
+};
