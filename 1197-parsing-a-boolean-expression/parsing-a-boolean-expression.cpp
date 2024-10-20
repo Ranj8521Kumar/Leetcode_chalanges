c652302a4db29dc808b5ac8407705cac0c2cacd7
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-10-21 00:29:59 +0530

    Time: 10 ms (44.83%) | Memory: 12.1 MB (15.28%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+
+        for (const auto &ch : expression) {
+            if (ch != ')' && ch != ',') {
+                st.push(ch);
+            } else if (ch == ')') {
+                vector<bool> exp;
+
+                while (!st.empty() && st.top() != '(') {
+                    char t = st.top();
+                    st.pop();
+
+                    if (t == 't') {
+                        exp.push_back(true);
+                    } else {
+                        exp.push_back(false);
+                    }
+                }
+
+                st.pop();
+
+                if (!st.empty()) {
+                    char t = st.top();
+                    st.pop();
+
+                    bool v = exp[0];
+
+                    if (t == '&') {
+                        for (auto b : exp) {
+                            v = v & b;
+                        }
+                    } else if (t == '|') {
+                        for (auto b : exp) {
+                            v = v | b;
+                        }
+                    } else {
+                        v = !exp[0];
+                    }
+
+                    if (v) {
+                        st.push('t');
+                    } else {
+                        st.push('f');
+                    }
+                }
+            }
+        }
+        return st.top() == 't';
+    }
+};
