5642eb95d70f278fcfe8742bc26350a7ac223a89
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-10-31 00:57:07 +0530

    Time: 59 ms (56.06%) | Memory: 14.9 MB (67.26%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -4,29 +4,31 @@ public:
         int n = nums.size();
         vector<int> LIS(n, 1), LDS(n, 1);
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    LIS[i] = max(LIS[i], LIS[j] + 1);
+        //calculating LIS
+        for(int i = 0; i<n; i++){
+            for(int j= i-1; j>=0; j--){
+                if(nums[i]>nums[j]){
+                    LIS[i] = max(LIS[i], LIS[j]+1);
                 }
             }
         }
 
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = n - 1; j > i; --j) {
-                if (nums[i] > nums[j]) {
-                    LDS[i] = max(LDS[i], LDS[j] + 1);
+        //calculating LDS
+        for(int i = n-1; i>=0; i--){
+            for(int j = i+1; j<n; j++){
+                if(nums[i]>nums[j]){
+                    LDS[i] = max(LDS[i], LDS[j]+1);
                 }
             }
         }
 
-        int maxMountainLength = 0;
-        for (int i = 1; i < n - 1; ++i) {
-            if (LIS[i] > 1 && LDS[i] > 1) {  
-                maxMountainLength = max(maxMountainLength, LIS[i] + LDS[i] - 1);
+        //calculate MinRemoval
+        int minRemoval = n;
+        for(int i = 0; i<n; i++){
+            if(LIS[i]>1 && LDS[i]>1){
+                minRemoval = min(minRemoval, (((i+1) - LIS[i]) + ((n-i) - LDS[i])));
             }
         }
-
-        return n - maxMountainLength;
+        return minRemoval;
     }
 };
