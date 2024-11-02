c6814d04f07b9612ea00de9df81389572f32b15a
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-11-03 00:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (25.43%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.size();
+
+        char first = sentence[0];
+        char last = sentence[n-1];
+
+        if(first != last){
+            return false;
+        }
+
+        stringstream ss(sentence);
+        string word;
+        int cnt = 0;
+        char fst;
+        char lst;
+        while(ss >> word){
+            int n = word.size();
+            cnt++;
+            if(cnt>1){
+                fst = word[0];
+                if(fst != lst){
+                    return false;
+                }
+            }else{
+                fst = word[0];
+                lst = word[n-1];
+            }
+            lst = word[n-1];
+        }
+
+        return true;
+    }
+};
