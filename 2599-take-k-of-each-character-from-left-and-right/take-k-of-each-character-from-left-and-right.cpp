1119750a54bfdb2ddd8e815747ad6c944b26b3c1
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-11-21 01:56:03 +0530

    Time: 11 ms (71.48%) | Memory: 12 MB (91.48%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -2,27 +2,53 @@ class Solution {
 public:
     int takeCharacters(string s, int k) {
         // Total counts
-        vector<int> count(3, 0);
-        for (char c : s) {
-            count[c - 'a']++;
+        int n = s.size();
+        int count_a  = 0;
+        int count_b = 0;
+        int count_c = 0;
+
+        for(const auto &it: s){
+            if(it == 'a'){
+                count_a++;
+            }else if(it == 'b'){
+                count_b++;
+            }else{
+                count_c++;
+            }
         }
-        
-        if (*min_element(count.begin(), count.end()) < k) {
+
+        if(count_a<k || count_b<k || count_c<k){
             return -1;
         }
-        
-        // Sliding Window
-        int res = INT_MAX;
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            count[s[r] - 'a']--;
-            
-            while (*min_element(count.begin(), count.end()) < k) {
-                count[s[l] - 'a']++;
-                l++;
+
+        int i = 0;
+        int j = 0;
+        int notDeletedWindow = 0;
+
+        while(j<n){
+            char ch = s[j];
+            if(ch == 'a'){
+                count_a--;
+            }else if(ch == 'b'){
+                count_b--;
+            }else{
+                count_c--;
             }
-            res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+            
+            while(i<=j && (count_a<k || count_b<k || count_c<k)){
+                char ch = s[i];
+                if(ch == 'a'){
+                    count_a++;
+                }else if(ch == 'b'){
+                    count_b++;
+                }else{
+                    count_c++;
+                }
+                i++;   
+            }
+            notDeletedWindow = max(notDeletedWindow, j-i+1);
+            j++;
         }
-        return res;
+        return n - notDeletedWindow;
     }
 };
