696c2e6752570e8155a9d92a273e520a62ae3355
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-01 23:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (17.49%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i != j && arr[i] == 2 * arr[j]) {
+                if (arr[i] == 2 * arr[j] && i!=j) {
                     return true;
                 }
             }
