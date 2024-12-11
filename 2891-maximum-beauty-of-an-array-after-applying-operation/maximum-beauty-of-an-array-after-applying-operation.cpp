ab75732636a8568229c02a5ff73e4baf54acd484
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-12 03:06:04 +0530

    Time: 51 ms (60.74%) | Memory: 105 MB (84.62%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,39 +1,23 @@
 class Solution {
-private:
-    int binarySearch(vector<int> &nums, int y){
-        int left = 0;
-        int right = nums.size() - 1;
-
-        int result;
-
-        while(left<=right){
-            int mid = left + (right - left)/2;
-
-            if(nums[mid]<=y){
-                result = mid;
-                left  = mid+1; //for farthest point from x, which satisfy the condition for y<=x+2*k
-            }else{
-                right = mid - 1;
-            }
-        }
-        return result;
-    }
-
-
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+        int n=nums.size();
 
         int maxBeauty = 0;
 
-        int n = nums.size();
+        sort(begin(nums), end(nums));
+
+        int j = 0;
+
         for(int i = 0; i<n; i++){
             int x = nums[i];
-            int y = x + 2*k;
+            int y = x+2*k;
 
-            int j = binarySearch(nums, y);
+            while(j<n && nums[j]<=y){
+                j++;
+            }
 
-            maxBeauty = max(maxBeauty, j - i + 1);
+            maxBeauty = max(maxBeauty, j-i);
         }
 
         return maxBeauty;
