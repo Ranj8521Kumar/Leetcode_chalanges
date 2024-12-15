e082d500d7ada91129139fdda4bcccfa28705236
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-16 01:14:45 +0530

    Time: 289 ms (89.26%) | Memory: 97.9 MB (41.09%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    #define p pair<double, int> //{gain, index}
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        int n = classes.size();
+
+        priority_queue<p> maxHeap;
+
+        int index = 0;
+        for(auto &vec: classes){
+            double ratio = (double)vec[0]/vec[1];
+            double updatedRatio = (double)(vec[0]+1)/(vec[1]+1);
+
+            double delta = updatedRatio - ratio;
+
+            maxHeap.push({delta, index});
+            index++;
+        }
+
+        while(extraStudents--){
+            auto curr = maxHeap.top();
+            maxHeap.pop();
+
+            int idx  = curr.second;
+
+            classes[idx][0] += 1;
+            classes[idx][1] += 1;
+
+            double ratio = (double)classes[idx][0]/classes[idx][1];
+            double updatedRatio = (double)(classes[idx][0]+1)/(classes[idx][1]+1);
+
+            double delta = updatedRatio - ratio;
+
+            maxHeap.push({delta, idx});
+        }
+
+        double result = 0.0;
+        for(auto &vec: classes){
+            result += (double)vec[0]/vec[1];
+        }
+
+        return result/n;
+    }
+};
