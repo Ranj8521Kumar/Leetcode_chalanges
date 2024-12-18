c9f3a8248573f3329d44291e00d73d312f3fa653
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-19 04:12:43 +0530

    Time: 3 ms (75.70%) | Memory: 18.4 MB (37.62%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
-    void BFS(unordered_map<int, vector<int>>& adj, vector<int>& indegree, vector<int>& result, int v){
-        queue<int> que;
-        int count = 0;
+    bool DFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& recVisited, stack<int>& st){
+        visited[u] = true;
+        recVisited[u] = true;
 
-        for(int u = 0; u<v; u++){
-            if(indegree[u] == 0){
-                que.push(u);
-                count++;
-            }
-        }
-
-        while(!que.empty()){
-            int u = que.front();
-            result.push_back(u);
-            que.pop();
-
-            for(auto &v: adj[u]){
-                indegree[v]--;
-
-                if(indegree[v] == 0){
-                    que.push(v);
-                    count++;
+        for(auto &v: adj[u]){
+            if(visited[v] && recVisited[v]){
+                return true;
+            }else{
+                if(!visited[v]){
+                    if(DFS(adj, v, visited, recVisited, st)){
+                        return true;
+                    }
                 }
             }
         }
-
-        if(count != v){
-            result  = {};
-        }
+        st.push(u);
+        recVisited[u] = false;
+        return false;
     }
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //using dfs
         int v = numCourses;
         int n = prerequisites.size();
 
         vector<int> result;
 
-        vector<int> indegree(v, 0);
-        unordered_map<int, vector<int>> adj;
+        vector<bool> visited(v, false);
+        vector<bool> recVisited(v, false);
+
+        vector<vector<int>> adj(v, vector<int> ());
 
         for(auto &vec: prerequisites){
             int u = vec[1];
             int v = vec[0];
 
             adj[u].push_back(v);
-
-            indegree[v]++;
         }
 
-        BFS(adj, indegree, result, v);
+        stack<int> st;
+        for(int u = 0; u<v; u++){
+            if(!visited[u]){
+                if(DFS(adj, u, visited, recVisited, st)){
+                    return {};
+                };
+            }
+        }
+
+        while(!st.empty()){
+            int u = st.top();
+            st.pop();
+
+            result.push_back(u);
+        }
 
         return result;
     }
