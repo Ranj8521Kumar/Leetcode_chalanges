c8c89856e0750e804d84ca162a68a9a4c717ef04
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-20 02:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.11%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -3,24 +3,20 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
 
-        vector<int> suffixMin(n, INT_MAX);
-        int minValue = INT_MAX;
+        int chunk = 0;
+        int cummulativeSum = 0;
+        int sum = 0;
 
-        for(int i = n-1; i>=0;  i--){
-            minValue = min(minValue, arr[i]);
-            suffixMin[i] =  minValue;
-        }
-
-        int maxValue;
-        int count = 0;
-        
         for(int i = 0; i<n; i++){
-            maxValue = (i == 0)?-1:max(maxValue, arr[i-1]);
-            if(maxValue  <= suffixMin[i]){
-                count++;
+            cummulativeSum += i;
+
+            sum += arr[i];
+
+            if(cummulativeSum == sum){
+                chunk++;
             }
         }
 
-        return count;
+        return chunk;
     }
 };
