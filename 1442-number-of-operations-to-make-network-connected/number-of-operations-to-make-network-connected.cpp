0efcd97b7121bed50c7e5923d265c9fc3fa25dfa
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-21 05:01:15 +0530

    Time: 43 ms (22.14%) | Memory: 72 MB (5.02%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
-    int find(int u, vector<int>& parent){
-        if(u  == parent[u]){
-            return u;
-        }
+    void BFS(vector<vector<int>>& adj, int u, vector<bool>& visited){
+        queue<int> que;
+        que.push(u);
+        visited[u] = true;
 
-        return parent[u] = find(parent[u], parent);
-    }
-
-    void unionSet(int u, int v, vector<int>& parent, int &components){
-        int parent_u = find(u, parent);
-        int parent_v = find(v, parent);
-
-        if(parent_u != parent_v){
-            parent[parent_v] = parent[u];
-            components--;
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            
+            for(auto &v: adj[u]){
+                if(!visited[v]){
+                    que.push(v);
+                    visited[v] = true;
+                }
+            }
         }
     }
 
     int makeConnected(int n, vector<vector<int>>& connections) {
         int v = n;
-
         int edges = connections.size();
 
         if(edges < v-1) return -1;
 
-        vector<int> parent(v);
+        vector<vector<int>> adj(v, vector<int> ());
 
-        for(int u = 0; u<v; u++){
-            parent[u] = u;
-        }
-
-        int components = v;
+        int components = 0;
 
         for(auto &edge: connections){
             int u = edge[0];
             int v = edge[1];
 
-            unionSet(u, v, parent, components);
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(v, false);
+
+        for(int u = 0; u<v; u++){
+            if(!visited[u]){
+                components++;
+                BFS(adj, u, visited);
+            }
         }
 
         return components - 1;
