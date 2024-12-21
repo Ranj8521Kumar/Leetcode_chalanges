30ac207cd31258ef1c2a3f538618f2b9266704f5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-22 02:05:51 +0530

    Time: 171 ms (29.45%) | Memory: 178.5 MB (41.72%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    long DFS(vector<vector<int>>& adj, int u, int parentNode, int k, vector<int>& values, int &componentsCount){
-        long sum = 0;
-        for(auto &v: adj[u]){
-            if(v != parentNode){
-                sum += DFS(adj, v, u, k, values, componentsCount);
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int>> adj(n);
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n, false);
+        int count = 0;
+
+        function<long long(int)> dfs = [&](int node) {
+            visited[node] = true;
+            long long sum = values[node];
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    sum += dfs(neighbor);
+                }
+            }
+
+            if (sum % k == 0) {
+                ++count;
+                return 0LL;
+            }
+
+            return sum;
+        };
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i);
             }
         }
 
-        sum += values[u];
-        if(sum % k == 0){
-            componentsCount++;
-            sum = 0;
-        }
-
-        return sum;
-    }
-
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        int v =  n;
-        vector<vector<int>> adj(v);
-        
-        for(auto &edge: edges){
-            int u = edge[0];
-            int v = edge[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        int componenetsCount = 0;
-        DFS(adj, 0, -1, k, values, componenetsCount);
-
-        return componenetsCount;
+        return count;
     }
 };
