17200238ae380de440c57dccab3acbeaebc133ac
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-23 02:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (11.47%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
+        //using monotonic stack
+        //and optimixed approach
 
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(prices[j] <= prices[i]){
-                    prices[i] -= prices[j];
-                    break;
-                }
+        int n = prices.size();
+        stack<int> st;
+        vector<int> result(n);
+
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && st.top() > prices[i]){
+                st.pop();
             }
+
+            if(!st.empty()){
+                result[i] = prices[i] - st.top();
+            }else{
+                result[i] = prices[i];
+            }
+
+            st.push(prices[i]);
         }
 
-        return prices;
+        return result;
     }
 };
