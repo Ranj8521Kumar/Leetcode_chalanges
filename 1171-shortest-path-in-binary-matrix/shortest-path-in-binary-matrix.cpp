57e156fee040234bc4a35b27f9494d4ff9b9c142
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-22 08:13:10 +0530

    Time: 35 ms (38.42%) | Memory: 23.9 MB (66.98%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,55 +1,57 @@
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> p;
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
-        vector<vector<int>> directions = {{0, 1}, {1, 0}, {1,1}, {-1, 1}, {-1,0}, {-1, -1}, {0, -1},  {1, -1}};
 
         if(m <= 0 || n <= 0 || grid[0][0] == 1){
             return -1;
         }
 
-        auto isSafe = [&](int i, int j){
-            if(i<0 || j<0 || i>=m || j>=n){
+        vector<vector<int>> direction = {{0, 1}, {1, 0}, {1, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0,  -1}, {1, -1}};
+
+        // Lambda function for isSafe
+        auto isSafe = [&](int i,  int j){
+            if(i < 0 || j < 0 || i >= m || j>= n){
                 return false;
             }
 
             return true;
         };
 
-        int lebel = 0;
-        queue<pair<int, int>> que;
-        que.push({0, 0});
-        grid[0][0] = 1;
+        vector<vector<int>> result(m, vector<int> (n, INT_MAX));
 
-        while(!que.empty()){
-            int len = que.size();
+        priority_queue<p, vector<p>, greater<p>> pq;
+        pq.push({0, {0, 0}});
+        result[0][0] = 0;
 
-            while(len--){
-                auto u = que.front();
-                que.pop();
-                int i = u.first;
-                int j = u.second;
-                
-                if(i == m-1 && j == n-1){
-                    return lebel + 1;
-                }
+        int dist = 1;
 
-                for(auto &dir: directions){
-                    int i = u.first + dir[0];
-                    int j = u.second + dir[1];
+        while(!pq.empty()){
+            auto u = pq.top();
+            pq.pop();
 
-                    if(isSafe(i, j) && grid[i][j] != 1){
-                        grid[i][j] = 1;
-                        que.push({i, j});
-                    }
+            int i = u.second.first;
+            int j = u.second.second;
+            int d = u.first;
+
+            for(auto &dir: direction){
+                int new_i = i + dir[0];
+                int new_j = j + dir[1];
+
+                if(isSafe(new_i, new_j) && grid[new_i][new_j] == 0 && (d + dist) < result[new_i][new_j]){
+                    pq.push({(d + dist), {new_i, new_j}});
+                    result[new_i][new_j] = d + dist;
                 }
             }
-
-            lebel++;
         }
 
-        return -1;
+        if(result[m-1][n-1] == INT_MAX){
+            return -1;
+        }
+
+        return result[m-1][n-1] + 1;
     }
 };
