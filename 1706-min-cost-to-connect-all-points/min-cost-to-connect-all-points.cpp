451f4dc1b8d2f6c52543347a60a75136ff3d4d45
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-24 06:40:14 +0530

    Time: 1588 ms (7.35%) | Memory: 321.2 MB (6.24%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
+    int find(int u, vector<int>& parent){
+        if(u == parent[u]){
+            return u;
+        }
+
+        return parent[u] = find(parent[u], parent);
+    }
+
+    void unionSet(int u, int v, vector<int>& parent){
+        int parent_u = find(u, parent);
+        int parent_v = find(v, parent);
+
+        if(parent_u != parent_v){
+            parent[parent_u] = parent_v;
+        }
+    }
+
     int minCostConnectPoints(vector<vector<int>>& points) {
         int v = points.size();
 
-        vector<vector<pair<int, int>>> adj(v);
+        int result = 0;
+
+        vector<vector<int>> adj;
         for(int i = 0; i<v; i++){
             for(int j = i+1; j<v; j++){
                 auto point1 = points[i];
@@ -11,36 +30,29 @@ public:
 
                 int wt = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1]);
 
-                adj[i].push_back({j, wt});
-                adj[j].push_back({i, wt});
+                adj.push_back({i, j, wt});
             }
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<bool> visited(v, false);
-        pq.push({0, 0});
+        sort(adj.begin(), adj.end(), [](vector<int>& a, vector<int>& b){
+            return a[2] < b[2];
+        });
 
-        int result = 0;
+        int len = adj.size();
 
-        while(!pq.empty()){
-            auto u = pq.top();
-            pq.pop();
+        vector<int> parent(v);
+        for(int u = 0; u<v; u++){
+            parent[u] = u;
+        }
 
-            int node = u.second;
-            int wt = u.first;
+        for(auto &edge: adj){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
 
-            if(visited[node]) continue;
-
-            result += wt;
-            visited[node] = true;
-
-            for(auto &v: adj[node]){
-                int neighbour = v.first;
-                int neighbour_wt = v.second;
-
-                if(!visited[neighbour]){
-                    pq.push({neighbour_wt, neighbour});
-                }
+            if(find(u, parent) != find(v, parent)){
+                result += wt;
+                unionSet(u, v, parent);
             }
         }
 
