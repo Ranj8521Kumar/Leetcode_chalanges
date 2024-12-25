d158a6d0e191843b8fa9aab558cb9c0f78406be4
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 04:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (45.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-//using buttom - up approach
+//using buttom - up approach with constant space complexity
     int rob(vector<int>& nums) {
         int n = nums.size();
         
         if(n == 1) return nums[0];//if only one house
 
         //steal first house - have we not taken last house
-        vector<int> t(n+1, -1);
-        t[0] =  0;
-        t[1] = nums[0];
+        int prevPrev =  0;
+        int prev = nums[0];
+        int maxProfit = max(nums[0], nums[1]);
 
         for(int i = 2; i<=n-1; i++){//if steal first house
-            int steal = nums[i-1] + (t[i-2]);
-            int skip = t[i-1]; 
+            int steal = nums[i-1] + prevPrev;
+            int skip = prev; 
 
-            t[i] = max(steal, skip);
+            maxProfit = max(steal, skip);
+
+            prevPrev = prev;
+            prev = maxProfit;
         }
 
-        int stealFirstHouse = t[n-1];
+        int stealFirstHouse = maxProfit;
 
         //skip first house - have we taken last house
-        t = vector<int> (n+1, -1);
-        t[0] =  0;
-        t[1] = 0; //since sip first house
+        prevPrev =  0;
+        prev = 0; //since sip first house
 
         for(int i = 2; i<=n; i++){
-            int steal = nums[i-1] + (t[i-2]);
-            int skip = t[i-1];
+            int steal = nums[i-1] + (prevPrev);
+            int skip = prev;
 
-            t[i] = max(steal, skip);
+            maxProfit = max(steal, skip);
+
+            prevPrev = prev;
+            prev = maxProfit;
         }
 
-        int skipFirstHouse = t[n];
+        int skipFirstHouse = maxProfit;
 
         return max(stealFirstHouse, skipFirstHouse);
     }
