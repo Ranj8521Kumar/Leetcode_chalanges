fe36d693952d76b808743662ad01e22207897fa4
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 03:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.23%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-//recursion with memoization
-    int t[101];
-
-    int solve(int i, int n, vector<int>& nums){
-        if(i > n) return 0;
-
-        if(t[i] != -1) return t[i];
-
-        int steal = nums[i] + solve(i+2, n, nums);
-        int skip = solve(i+1, n, nums);
-
-        return t[i] = max(steal, skip);
-    }
-
+//using buttom - up approach
     int rob(vector<int>& nums) {
         int n = nums.size();
+        
+        if(n == 1) return nums[0];
 
-        if(n == 1) return nums[0];//special case, if only one house
+        vector<int> t(n+1, -1);
+        t[0] =  0;
 
-        memset(t, -1, sizeof(t));
-        int withZeroIindexedHouse = solve(0, n-2, nums);// can steal till n-2 indexed house
-        memset(t,-1, sizeof(t));
-        int withOutZeroIindexedHouse = solve(1, n-1, nums);// can steal till n-1 indexed house
+        for(int i = 1; i<=n-1; i++){//if steal first house
+            int steal = nums[i-1] + ((i>=2)?t[i-2]:0);
+            int skip = t[i-1]; 
 
-        return max(withZeroIindexedHouse, withOutZeroIindexedHouse);
+            t[i] = max(steal, skip);
+        }
+
+        int stealFirstHouse = t[n-1];
+
+        //skip first house - have we taken last house
+        t = vector<int> (n+1, -1);
+        t[0] =  0;
+        t[1] = 0; //since sip first house
+
+        for(int i = 2; i<=n; i++){
+            int steal = nums[i-1] + (t[i-2]);
+            int skip = t[i-1];
+
+            t[i] = max(steal, skip);
+        }
+
+        int skipFirstHouse = t[n];
+
+        return max(stealFirstHouse, skipFirstHouse);
     }
 };
