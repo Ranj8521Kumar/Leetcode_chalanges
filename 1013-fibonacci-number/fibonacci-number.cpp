cb4292f39e8a7cfb0c9006d09e08097046d53dae
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-25 08:48:19 +0530

    Time: 3 ms (62.84%) | Memory: 7.8 MB (32.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-//using buttom-up approach
+//using buttom-up approach with constant space complexity
     int fib(int n) {
-        if(n<=1) return n;
+        if(n <= 1) return n;
 
-        vector<int> dp(n+1, -1);
+        int a = 0; 
+        int b = 1;
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int c;
 
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        //total number of iteration equal to (n-1)
+        for(int i = 1; i<n; i++){
+            c = a+b;
+            a = b;
+            b = c;
         }
 
-        return dp[n];
+        return c;
     }
 };
