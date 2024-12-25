8c2542163da254e693763212043a36040a84c81f
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-25 12:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.71%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-//recursion + memoization
-    int t[101];
-    int solve(vector<int>& nums, int i, int n){
-        if(i>=n) return 0;
+// Buttom - Up Apprach
+    int solve(vector<int>& nums, vector<int>& arr, int n){
+        if(n==0) return 0;
 
-        if(t[i] != -1) return t[i];
+        arr[0] = 0;
+        arr[1] = nums[0];
 
-        int steal = nums[i] + solve(nums, i+2, n);
-        int skip = solve(nums, i+1, n);
+        for(int i = 2; i<=n; i++){
+            int steal = nums[i-1] + arr[i-2];
+            int skip = arr[i-1];
 
-        return t[i] = max(steal, skip);
+            arr[i] = max(steal, skip);
+        }
+
+        return arr[n];
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, n);
+        vector<int> arr(n+1);
+
+        return solve(nums, arr, n);
     }
 };
