d26a1ff2b4274c0bb1b37ec334db3034b603473b
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-25 07:38:01 +0530

    Time: 775 ms (20.88%) | Memory: 429.6 MB (10.12%) - LeetSync

--- 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,47 +1,64 @@
 class Solution {
 public:
-    pair<int, int> DFS(vector<vector<int>>& adj, int u, int parent) {
+    pair<int, int> BFS(unordered_map<int, vector<int>>& adj, int u){
+        unordered_map<int, bool> visited;
         int maxDist = 0;
         int farthestNode = u;
+        queue<int> que;
 
-        for (auto &v : adj[u]) {
-            if (v != parent) {
-                auto temp = DFS(adj, v, u);
-                if (temp.first + 1 > maxDist) {
-                    maxDist = temp.first + 1;
-                    farthestNode = temp.second;
+        que.push(u);
+        visited[u] = true;
+
+        while(!que.empty()){
+            int len = que.size(); //size of current level
+
+            while(len--){
+                int u = que.front();
+                que.pop();
+                farthestNode = u;
+
+                //adding node of next level
+                for(auto &v: adj[u]){
+                    if(!visited[v]){
+                        visited[v] = true;
+                        que.push(v);
+                    }
                 }
+            }
+
+            if(!que.empty()){// if que empty then there is no any level
+                maxDist++;
             }
         }
 
         return {maxDist, farthestNode};
     }
 
-    int treeDiameter(vector<vector<int>>& edges, int n) {
-        vector<vector<int>> adj(n);
-        for (auto &edge : edges) {
+
+    int treeDiameter(vector<vector<int>>& edges){
+        unordered_map<int, vector<int>> adj;
+
+        for(auto &edge: edges){
             int u = edge[0];
             int v = edge[1];
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        auto temp1 = DFS(adj, 0, -1); // Get farthest node from 0
-        auto temp2 = DFS(adj, temp1.second, -1); // Get farthest node from temp1
-        return temp2.first; // Diameter of the tree
+        auto [maxDist, farthestNode] = BFS(adj, 0);//find one end of the diameter
+        auto [diameter, _] = BFS(adj, farthestNode);//find length of the diameter or second length of  the diameter
+
+        return diameter;
     }
 
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        int n = edges1.size() + 1; // Nodes in tree 1
-        int m = edges2.size() + 1; // Nodes in tree 2
+        int d1 = treeDiameter(edges1);
+        int d2 = treeDiameter(edges2);
 
-        int diameter1 = treeDiameter(edges1, n);
-        int diameter2 = treeDiameter(edges2, m);
+        int half1 = (d1+1)/2;
+        int half2 = (d2+1)/2;
 
-        // Merging increases the diameter minimally by connecting the trees
-        int half1 = (diameter1 + 1) / 2; // Half of tree 1's diameter
-        int half2 = (diameter2 + 1) / 2; // Half of tree 2's diameter
-
-        return max({diameter1, diameter2, half1 + half2 + 1});
+        return max({d1, d2, half1 + half2 + 1});
     }
 };
