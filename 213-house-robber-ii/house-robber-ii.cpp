8b750139883e68fe91c49ee60b2780b30b34e281
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 03:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (11.86%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -4,13 +4,15 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        if(n == 1) return nums[0];
+        if(n == 1) return nums[0];//if only one house
 
+        //steal first house - have we not taken last house
         vector<int> t(n+1, -1);
         t[0] =  0;
+        t[1] = nums[0];
 
-        for(int i = 1; i<=n-1; i++){//if steal first house
-            int steal = nums[i-1] + ((i>=2)?t[i-2]:0);
+        for(int i = 2; i<=n-1; i++){//if steal first house
+            int steal = nums[i-1] + (t[i-2]);
             int skip = t[i-1]; 
 
             t[i] = max(steal, skip);
