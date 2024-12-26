b0d28dcec03eabe7c3396c461a3b4e8e6d41a63e
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 06:12:59 +0530

    Time: 132 ms (74.00%) | Memory: 96.4 MB (77.63%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -10,9 +10,9 @@ public:
         //1 -> odd
 
         for(int i = 1; i<n+1; i++){
-            t[i][0] = max((t[i-1][1] + nums[i-1]), t[i-1][0]);//for even length
+            t[i][0] = max((t[i-1][1] - nums[i-1]), t[i-1][0]);//for even length
 
-            t[i][1] = max((t[i-1][0] - nums[i-1]), t[i-1][1]);//for odd length
+            t[i][1] = max((t[i-1][0] + nums[i-1]), t[i-1][1]);//for odd length
         }
 
         return max(t[n][0], t[n][1]);
