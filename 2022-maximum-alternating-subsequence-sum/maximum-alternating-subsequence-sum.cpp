285d36370268f9d614f0962b936b478c880bc5bf
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 06:08:30 +0530

    Time: 124 ms (80.25%) | Memory: 96.5 MB (77.63%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    //recursion + memoization
-    long long t[100001][2];
-    long long solve(vector<int>& nums, int n, int i, bool isEven) {
-        if (i >= n) {
-            return 0;
-        }
-
-        if(t[i][isEven] != -1) return t[i][isEven];
-        
-        long long take = (isEven?nums[i]:-nums[i]) + solve(nums, n, i + 1, !isEven);
-        long long notTake = solve(nums, n, i + 1, isEven);
-
-        return t[i][isEven] = max(take, notTake);
-    }
-
+//using buttom - Approach
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
 
-        if (n == 1) {
-            return nums[0];
+        long long t[100001][2];
+
+        //0 -> even
+        //1 -> odd
+
+        for(int i = 1; i<n+1; i++){
+            t[i][0] = max((t[i-1][1] + nums[i-1]), t[i-1][0]);//for even length
+
+            t[i][1] = max((t[i-1][0] - nums[i-1]), t[i-1][1]);//for odd length
         }
 
-        memset(t, -1, sizeof(t));
-
-        return solve(nums, n, 0, true);
+        return max(t[n][0], t[n][1]);
     }
 };
