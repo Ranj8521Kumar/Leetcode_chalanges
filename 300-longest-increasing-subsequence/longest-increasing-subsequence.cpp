8d09596b438484f112aa35afa9cda064c7b29dfe
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-26 07:32:01 +0530

    Time: 48 ms (72.29%) | Memory: 14.5 MB (36.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-//using recursion + brute-force
-    int n;
-    int t[2501][2501];
-
-    int solve(int i, int p, vector<int>& nums){
-        if(i >= n){
-            return 0;
-        }
-
-        if(p != -1 && t[i][p] != -1) return t[i][p];
-
-        //take
-        int take = 0;
-        if(p == -1 || nums[i] > nums[p]){
-            take = 1 + solve(i+1, i,  nums);
-        }
-
-        //skip
-        int  skip = solve(i+1, p, nums);
-        
-        if(p != -1){
-            t[i][p] =  max(take, skip);
-        }
-
-        return max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        memset(t, -1, sizeof(t));
+        int n = nums.size();
 
-        return solve(0, -1, nums);
+        if(n == 0) return 0;
+
+        vector<int> dp(n+1, 1);
+        int longest = 1;
+
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+
+            longest = max(longest, dp[i]);
+        }
+
+        return  longest;
     }
 };
