41598c02c559228a084ff3a89b7fb79dcf140de4
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2024-12-28 02:28:14 +0530

    Time: 5 ms (14.86%) | Memory: 49.7 MB (6.58%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-//Optimal Approach
     int maxScoreSightseeingPair(vector<int>& values) {
+        //using maxHeap or Priority Queue
+        priority_queue<int> maxHeap; //since by default prioroty queue is a max Heap
         int n = values.size();
         int maxScore = INT_MIN;
 
-        int maxScoreTill = values[0];//store the maximum value till of valuse[i] + i till ith index
         for(int i = 1; i<n; i++){
-            maxScoreTill = max(maxScoreTill, values[i-1] + (i-1));
-            int value = values[i] - i;
+            maxHeap.push(values[i-1] + (i-1));
             
-            maxScore = max(maxScore, maxScoreTill + value);
+            int maxValue =  maxHeap.top();
+            maxScore = max(maxScore, maxValue + (values[i] - i));
         }
 
         return maxScore;
