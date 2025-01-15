608458e747f9a65d42f6f604b85adf546bc225b6
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-01-16 00:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (64.02%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int x = num1;
-        int setBitX = __builtin_popcount(x);
-        int setBitNum2 = __builtin_popcount(num2);
+        int x = 0;
 
-        if(setBitX == setBitNum2){//case1: setBitX = setBitNum2
-            return x;
-        }else if(setBitX < setBitNum2){//case2: setBitX < setBitNum2
-            int count = setBitNum2 - setBitX;
-            for(int i = 0; i<=31; i++){
-                if((x & (1<<i)) == 0){
-                    x |= (1<<i);
-                    count--;
-                }
+        int requiredSetBits = __builtin_popcount(num2);
 
-                if(count == 0){
-                    return x;
-                }
-            }
-        }else{//case3: setBitX > setBitNum2
-            int count = setBitX - setBitNum2;
-            for(int i = 0; i<=31; i++){
-                if((x & (1<<i)) > 0){
-                    x ^= (1<<i);
-                    count--;
-                }
-
-                if(count == 0){
-                    return x;
-                }
+        for(int i = 31; i>=0 && requiredSetBits > 0; i--){
+            if((num1 & (1<<i)) > 0){
+                x |= (1<<i);
+                requiredSetBits--;
             }
         }
-        
-        return -1;
+
+        for(int i = 0; i<=31 && requiredSetBits > 0; i++){
+            if((num1 & (1<<i)) == 0){
+                x ^= ((1<<i));
+                requiredSetBits--;
+            }
+        }
+
+        return x;
     }
 };
