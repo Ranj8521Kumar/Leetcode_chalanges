de7475015b15b875d29aace3af381439ab93c5a5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-01-16 01:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.19%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -7,14 +7,14 @@ public:
 
         for(int i = 31; i>=0 && requiredSetBits > 0; i--){
             if((num1 & (1<<i)) > 0){
-                x |= (1<<i);
+                x |= ((1<<i));
                 requiredSetBits--;
             }
         }
 
         for(int i = 0; i<=31 && requiredSetBits > 0; i++){
             if((num1 & (1<<i)) == 0){
-                x ^= ((1<<i));
+                x |= ((1<<i));
                 requiredSetBits--;
             }
         }
