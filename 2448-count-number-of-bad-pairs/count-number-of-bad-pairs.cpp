0460e1c7ec97f6d2451f294f6155b76dc92e4b47
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-02-10 01:03:13 +0530

    Time: 65 ms (73.25%) | Memory: 87.6 MB (70.06%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -3,7 +3,7 @@ public:
     long long countBadPairs(vector<int>& nums) {
         unordered_map<int, int> mpp;
         long long goodPairs = 0;
-        int n = nums.size();
+        long long n = nums.size();
 
         for(int i = 0; i<n; i++){
             int key = nums[i] - i;
@@ -11,6 +11,6 @@ public:
             mpp[key]++;
         }
 
-        return ((1LL*n*(n-1))/2) - goodPairs;
+        return ((n*(n-1))/2) - goodPairs;
     }
 };
