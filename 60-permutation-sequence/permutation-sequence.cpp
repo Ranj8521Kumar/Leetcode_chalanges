4d1d04d7e1699194047c30e383ee8cf7928ca92a
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-20 01:45:00 +0530

    Time: 55 ms (15.04%) | Memory: 8.5 MB (76.19%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string str = "";
+        for(int i = 1; i<=n; i++){
+            str += to_string(i);
+        }
+
+        if(k == 1){
+            return str;
+        }
+
+        int cnt = 1;
+        while(next_permutation(str.begin(), str.end())){
+            cnt++;
+
+            if(cnt == k){
+                break;
+            }
+        }
+
+        return str;
+    }
+};
