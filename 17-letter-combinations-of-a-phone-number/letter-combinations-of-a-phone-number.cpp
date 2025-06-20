5965872b9d83cb7db6ae73c13e3d89f45b71d8d3
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-20 16:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (74.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        //Using backTrack approach
         int n = digits.size();
+
+        if(n == 0){
+            return {};
+        }
+
         vector<string> result;
         unordered_map<char, string> mpp;
         mpp['2'] = "abc";
@@ -13,56 +19,22 @@ public:
         mpp['8'] = "tuv";
         mpp['9'] = "wxyz";
 
+        string path;
 
-        // string comb = "";
-
-        // for(int i = 0; i<n; i++){
-        //     comb += mpp[digits[i]];
-        // }
-
-        if(n == 1){
-            string a = mpp[digits[0]];
-            for(auto &ch1: a){
-                result.push_back(string(1, ch1));
+        function<void(int)> backtrack = [&](int idx){
+            if(idx == n){
+                result.push_back(path);
+                return;
             }
-        }else if(n == 2){
-            string a = mpp[digits[0]];
-            string b = mpp[digits[1]];
 
-            for(auto &ch1: a){
-                for(auto &ch2: b){
-                    result.push_back(string(1, ch1) + ch2);
-                }
+            for(auto &ch: mpp[digits[idx]]){
+                path.push_back(ch);
+                backtrack(idx + 1);
+                path.pop_back();
             }
-        }else if(n == 3){
-            string a = mpp[digits[0]];
-            string b = mpp[digits[1]];
-            string c = mpp[digits[2]];
-
-            for(auto &ch1: a){
-                for(auto &ch2: b){
-                    for(auto &ch3: c){
-                        result.push_back(string(1, ch1) + ch2  + ch3);
-                    }
-                }
-            }
-        }else{
-            string a = mpp[digits[0]];
-            string b = mpp[digits[1]];
-            string c = mpp[digits[2]];
-            string d = mpp[digits[3]];
-
-            for(auto &ch1: a){
-                for(auto &ch2: b){
-                    for(auto &ch3: c){
-                        for(auto &ch4: d){
-                            result.push_back(string(1, ch1) + ch2  + ch3 + ch4);
-                        }
-                    }
-                }
-            }
-        }
-
+        };
+        
+        backtrack(0);
         return result;
     }
 };
