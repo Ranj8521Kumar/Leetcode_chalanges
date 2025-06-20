65db1903e38f1b06ff043cea69c1ea274d245988
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-21 01:34:58 +0530

    Time: 6 ms (48.80%) | Memory: 23.9 MB (58.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -16,7 +16,7 @@ public:
                 result.push_back({intervals[k][0], intervals[i][1]});
                 k  = i + 1;
             }else{
-                intervals[i+1][0] = min(intervals[i][0], intervals[i+1][0]);
+                
                 intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1]);
             }
         }
