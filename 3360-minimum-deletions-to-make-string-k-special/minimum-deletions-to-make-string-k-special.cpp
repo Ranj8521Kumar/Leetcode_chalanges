51fe0bc27c0163f34232620b23a3afd3e3d6012e
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-21 21:12:43 +0530

    Time: 16 ms (36.59%) | Memory: 19.8 MB (31.16%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//simple brurte force according to the frequencies
+//Slightly optimized
     int minimumDeletions(string word, int k) {
         int n = word.size();
 
@@ -13,23 +13,24 @@ public:
         for(auto &it: mpp){
             freq.push_back(it.second);
         }
+
+        sort(freq.begin(), freq.end());
+
         int result = INT_MAX;
         int m = freq.size();
+        int cummulativeFreq = 0;
 
         for(int i = 0; i<m; i++){
-            int deletion = 0;
+            int deletion = cummulativeFreq;
 
-            for(int j = 0; j<m; j++){
-                if(i == j) continue;
-            
-                if(freq[j] < freq[i]){
-                    deletion += freq[j];
-                }else if(abs(freq[j] - freq[i]) > k){
+            for(int j = m-1; j>i; j--){
+                if(abs(freq[j] - freq[i]) > k){
                     deletion += abs(freq[j] - freq[i] - k);
                 }
             }
 
             result = min(result, deletion);
+            cummulativeFreq += freq[i];
         }
 
         return result;
