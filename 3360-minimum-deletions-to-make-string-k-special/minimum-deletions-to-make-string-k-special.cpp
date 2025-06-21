33cbaa0a82d9de8fee1d57c8c4a76b0f78b2fe7a
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-21 21:17:08 +0530

    Update minimum-deletions-to-make-string-k-special.cpp

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -1,3 +1,43 @@
+//First Approach
+class Solution {
+public:
+//simple brurte force according to the frequencies
+    int minimumDeletions(string word, int k) {
+        int n = word.size();
+
+        unordered_map<char, int> mpp;
+        for(auto &ch: word){
+            mpp[ch]++;
+        }
+
+        vector<int> freq;
+        for(auto &it: mpp){
+            freq.push_back(it.second);
+        }
+        int result = INT_MAX;
+        int m = freq.size();
+
+        for(int i = 0; i<m; i++){
+            int deletion = 0;
+
+            for(int j = 0; j<m; j++){
+                if(i == j) continue;
+            
+                if(freq[j] < freq[i]){
+                    deletion += freq[j];
+                }else if(abs(freq[j] - freq[i]) > k){
+                    deletion += abs(freq[j] - freq[i] - k);
+                }
+            }
+
+            result = min(result, deletion);
+        }
+
+        return result;
+    }
+};
+
+//Second Approach
 class Solution {
 public:
 //Slightly optimized
