a208831e10d8fec4a65ce074659008b9f80997fd
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-06-22 21:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (44.22%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.cpp
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
     vector<string> divideString(string s, int k, char fill) {
-        int n = s.size();
-        vector<string> result;
+    //     int n = s.size();
+    //     vector<string> result;
 
-        int cnt = 0;
-        while(cnt < n){
-            result.push_back(s.substr(cnt, k));
-            cnt += k;
-        }
+    //     int cnt = 0;
+    //     while(cnt < n){
+    //         result.push_back(s.substr(cnt, k));
+    //         cnt += k;
+    //     }
 
-        if(cnt == n) return result;
+    //     if(cnt == n) return result;
 
-        cnt -= k;
-        result.pop_back(); //remove last incomplete pushed string
+    //     cnt -= k;
+    //     result.pop_back(); //remove last incomplete pushed string
 
-        string lastString = "";
-        while(cnt < n){
-            lastString += s[cnt];
-            cnt++;
-        }
+    //     string lastString = "";
+    //     while(cnt < n){
+    //         lastString += s[cnt];
+    //         cnt++;
+    //     }
 
-        while(lastString.size() < k){
-            lastString += fill;
-        }
+    //     while(lastString.size() < k){
+    //         lastString += fill;
+    //     }
 
-        result.push_back(lastString);
+    //     result.push_back(lastString);
 
-        return result;
+    //     return result;
+
+
+    //minimize space and time complexity
+    vector<string> result;
+    int n = s.size();
+    for(int i = 0; i<n; i+=k){
+        result.push_back(s.substr(i, min(n - i, k)));
+    }
+
+    int m = result.back().size();
+
+    if(m < k){
+        result.back().append((k - m), fill);
+    }
+
+    return result;
     }
 };
