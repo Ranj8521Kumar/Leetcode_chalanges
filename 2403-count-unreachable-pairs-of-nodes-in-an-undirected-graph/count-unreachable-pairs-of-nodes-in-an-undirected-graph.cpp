dfe7cf95c6f37b80b56083c859fbc7813be927c7
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-20 04:09:22 +0530

    Time: 35 ms (91.57%) | Memory: 151.7 MB (73.42%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,59 +1,48 @@
 class Solution {
 public:
-    int find(int u, vector<int>& parent){
+    int find(int u, vector<int> &parent){
         if(u == parent[u]){
             return u;
         }
 
-        return parent[u] = find(parent[u], parent);
+        return parent[u] =  find(parent[u], parent);
     }
 
-    void unionSet(int u, int v, vector<int>& parent, vector<int>& rank){
-        int parent_u = find(u, parent);
-        int parent_v = find(v, parent);
-
-        if(parent_u != parent_v){
-            if(rank[parent_u] == rank[parent_v]){
-                parent[parent_u] = parent_v;
-                rank[parent_u]++;
-            }else if(rank[parent_u] > rank[parent_v]){
-                parent[parent_v] = parent_u;
-            }else{
-                parent[parent_u] = parent_v;
-            }
+    void Union(int u, int v, vector<int> &parent){
+        if(u != v){
+            parent[u] = v;
         }
     }
 
-    long long countPairs(int n, vector<vector<int>>& edges) {   
-        //using DSU
-        int v = n;
-
-        vector<int> parent(v);
-        vector<int> rank(v, 0);
-
-        for(int u = 0; u<v; u++){
-            parent[u] = u;
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n, 0);
+        for(int i = 0; i<n; i++){
+            parent[i] = i;
         }
-        
+
         for(auto &edge: edges){
             int u = edge[0];
             int v = edge[1];
 
-            unionSet(u, v, parent, rank);
+            int u_parent = find(u, parent);
+            int v_parent = find(v, parent);
+
+            Union(u_parent, v_parent, parent);
         }
 
-        //count number of nodes in a component
-        unordered_map<int, int> count;
-        for(int u  = 0; u<v; u++){
-            count[find(u, parent)]++;
+
+        unordered_map<int, int> mpp;
+        for(int u = 0; u<n; u++){
+            mpp[find(u, parent)]++;
         }
 
-        long long remainingNode = v;
         long long result = 0;
+        long long remainingNodes = n;
 
-        for(auto &it: count){
-            remainingNode -= it.second;
-            result += it.second*remainingNode;
+        for(auto &it: mpp){
+            remainingNodes -= it.second;
+
+            result += it.second*remainingNodes;
         }
 
         return result;
