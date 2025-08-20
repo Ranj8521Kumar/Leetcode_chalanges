c82650b243bcfc5e0c1fe7b5399b436041a76d5e
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-21 00:46:58 +0530

    Time: 1484 ms (7.68%) | Memory: 307 MB (6.54%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,62 +1,47 @@
 class Solution {
 public:
-//using kruskal's Algorithm
-    int find(int u, vector<int>& parent){
-        if(u == parent[u]){
-            return u;
-        }
-
-        return parent[u] = find(parent[u], parent);
-    }
-
-    void unionSet(int u, int v, vector<int>& parent){
-        int parent_u = find(u, parent);
-        int parent_v = find(v, parent);
-
-        if(parent_u != parent_v){
-            parent[parent_u] = parent_v;
-        }
-    }
-
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int v = points.size();
+        //making the weighte graph
+        int V = points.size();
 
-        int result = 0;
+        unordered_map<int, vector<pair<int, int>>> adj;
+        for(int i = 0; i<V; i++){
+            for(int j = i+1; j<V; j++){
+                auto p1 = points[i];
+                auto p2 = points[j];
 
-        vector<vector<int>> adj;
-        for(int i = 0; i<v; i++){
-            for(int j = i+1; j<v; j++){
-                auto point1 = points[i];
-                auto point2 = points[j];
-
-                int wt = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1]);
-
-                adj.push_back({i, j, wt});
+                int wt = abs(p2[0] - p1[0]) + abs(p2[1] - p1[1]);
+                adj[i].push_back({j, wt});
+                adj[j].push_back({i, wt});
             }
         }
 
-        sort(adj.begin(), adj.end(), [](vector<int>& a, vector<int>& b){
-            return a[2] < b[2];
-        });
 
-        int len = adj.size();
+        vector<bool> visited(V, false);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        int minCost = 0;
 
-        vector<int> parent(v);
-        for(int u = 0; u<v; u++){
-            parent[u] = u;
-        }
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
 
-        for(auto &edge: adj){
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
+            int w = it.first;
+            int node = it.second;
 
-            if(find(u, parent) != find(v, parent)){
-                result += wt;
-                unionSet(u, v, parent);
+            if(visited[node] != true){
+                visited[node] = true;
+                minCost += w;
+
+                for(auto &temp: adj[node]){
+                    int v = temp.first;
+                    int weight = temp.second;
+
+                    pq.push({weight, v});
+                }
             }
         }
 
-        return result;
+        return minCost;
     }
 };
