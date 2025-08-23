d115c523c64e8fc8c5292a8f764a3ee97de1410a
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-24 02:06:45 +0530

    Time: 91 ms (39.56%) | Memory: 25.9 MB (20.88%) - LeetSync

--- 3459-find-the-minimum-area-to-cover-all-ones-ii/find-the-minimum-area-to-cover-all-ones-ii.cpp
+++ 3459-find-the-minimum-area-to-cover-all-ones-ii/find-the-minimum-area-to-cover-all-ones-ii.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // Functio for clockWiseRotation:
+    vector<vector<int>> clockWiseRotation(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> result(n, vector<int> (m));
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                result[j][m-i-1] = grid[i][j];
+            }
+        }
+
+        return result;
+    }
+
     // Function  to find minimun Area of a matrix containing all one's
     int minArea(vector<vector<int>>& grid, int rowStart, int rowEnd, int colStart, int colEnd){
         int m = grid.size();
@@ -27,7 +42,8 @@ public:
         return ((maxColumn - minColumn + 1)*(maxRow - minRow + 1));
     }
 
-    int minimumSum(vector<vector<int>>& grid) {
+    // Function for compute three non-verlapping triangle with minArea:
+    int computeTringlesWithMinArea(vector<vector<int>> &grid){
         int m = grid.size();
         int n = grid[0].size();
 
@@ -36,7 +52,7 @@ public:
         // 3 - Non-Overlapping rectangle -> 2 spilit lines
         // 6 cases
 
-        // first case:
+        // first and Second Cases:
         for(int rowSpilit = 1; rowSpilit < m; rowSpilit++){
             for(int colSpilit = 1; colSpilit < n; colSpilit++){
                 int topLeft = minArea(grid, 0, rowSpilit, 0, colSpilit);
@@ -44,12 +60,7 @@ public:
                 int down = minArea(grid, rowSpilit, m, 0, n);
 
                 result = min(result, (topLeft + topRight + down));
-            }
-        }
 
-        // Fourth Case:
-        for(int rowSpilit = 1; rowSpilit < m; rowSpilit++){
-            for(int colSpilit = 1; colSpilit < n; colSpilit++){
                 int buttomLeft = minArea(grid, rowSpilit, m, 0, colSpilit);
                 int buttomRight = minArea(grid, rowSpilit, m, colSpilit, n);
                 int top = minArea(grid, 0, rowSpilit, 0, n);
@@ -58,7 +69,7 @@ public:
             }
         }
 
-        // Second Case:
+        // Third Case:
         for(int rowSpilit1 = 1; rowSpilit1 < m; rowSpilit1++){
             for(int rowSpilit2 = rowSpilit1 + 1; rowSpilit2 < m; rowSpilit2++){
                 int top = minArea(grid, 0, rowSpilit1, 0, n);
@@ -69,38 +80,16 @@ public:
             }
         }
 
-        // Third Case:
-        for(int colSpilit1 = 1; colSpilit1 < n; colSpilit1++){
-            for(int colSpilit2 = colSpilit1 + 1; colSpilit2 < n; colSpilit2++){
-                int left = minArea(grid, 0, m, 0, colSpilit1);
-                int middle = minArea(grid, 0, m, colSpilit1, colSpilit2);
-                int right = minArea(grid, 0, m, colSpilit2, n);
+        return result;
+    }
 
-                result = min(result, (left + middle + right));
-            }
-        }
+    int minimumSum(vector<vector<int>>& grid) {
+        int result = INT_MAX;
+        result = min(result, computeTringlesWithMinArea(grid));
 
-        // Case Five:
-        for(int rowSpilit = 1; rowSpilit < m; rowSpilit++){
-            for(int colSpilit = 1; colSpilit < n; colSpilit++){
-                int left = minArea(grid, 0, m, 0, colSpilit);
-                int rightTop = minArea(grid, 0, rowSpilit, colSpilit, n);
-                int rightDown = minArea(grid, rowSpilit, m, colSpilit, n);
+        vector<vector<int>> rotatedGrid = clockWiseRotation(grid);
 
-                result = min(result, (left + rightTop + rightDown));
-            }
-        }
-
-        // Case Six:
-        for(int rowSpilit = 1; rowSpilit < m; rowSpilit++){
-            for(int colSpilit = 1; colSpilit < n; colSpilit++){
-                int leftTop = minArea(grid, 0, rowSpilit, 0, colSpilit);
-                int leftDown = minArea(grid, rowSpilit, m, 0, colSpilit);
-                int right = minArea(grid, 0, m, colSpilit, n);
-
-                result = min(result, (leftTop + leftDown + right));
-            }
-        }
+        result = min(result, computeTringlesWithMinArea(rotatedGrid));
 
         return result;
     }
