79f8e0eed6600592fa0f7220f049635f84c0410c
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-24 02:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (35.38%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int row = 0; row < m; row++){
+            for(int col = row; col < n; col++){
+                swap(matrix[row][col], matrix[col][row]); 
+            }
+        }
+
+        for(auto &row: matrix){
+            reverse(row.begin(), row.end());
+        }
+    }
+};
