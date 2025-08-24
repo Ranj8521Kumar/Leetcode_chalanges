87646e43050ed2f385250b687726e9ca4cf90202
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-25 03:02:06 +0530

    Time: 54 ms (20.15%) | Memory: 75.6 MB (36.29%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,25 +10,86 @@
  */
 class Solution {
 public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* mergeList(ListNode* head1, ListNode* head2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(head1 && head2){
+            if(head1->val <= head2->val){
+                temp->next = head1;
+                temp = head1;
+                head1 = head1->next;
+            }else{
+                temp->next = head2;
+                temp = head2;
+                head2 = head2->next;
+            }
+        }
+
+        while(head1){
+            temp->next = head1;
+            temp = head1;
+            head1 = head1->next;
+        }
+
+        while(head2){
+            temp->next = head2;
+            temp = head2;
+            head2 = head2->next;
+        }
+
+        return dummyNode->next;
+    }
+
     ListNode* sortList(ListNode* head) {
         //Simple Brute Force Approach:
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
+        // vector<int> arr;
+        // ListNode* temp = head;
+        // while(temp){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next;
+        // }
         
-        sort(arr.begin(), arr.end());
+        // sort(arr.begin(), arr.end());
 
-        int n = arr.size();
-        temp = head;
+        // int n = arr.size();
+        // temp = head;
 
-        for(int i = 0; i<n; i++){
-            temp->val = arr[i];
-            temp = temp->next;
-        }
+        // for(int i = 0; i<n; i++){
+        //     temp->val = arr[i];
+        //     temp = temp->next;
+        // }
 
-        return head;
+        // return head;
+
+
+        
+
+
+        // Let's aplly more optimized approach using merge sort:
+        if(head == nullptr || head->next ==  nullptr) return head;
+
+        ListNode* mid = findMiddle(head);
+        ListNode* right = mid->next;
+        mid->next = nullptr;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeList(left, right);
+
     }
 };
