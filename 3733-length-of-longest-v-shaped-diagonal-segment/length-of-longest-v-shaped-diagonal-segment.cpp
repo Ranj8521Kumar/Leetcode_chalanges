cdfc1275fa25527caed4d5cb6b2eb1da4c147efc
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-28 01:43:48 +0530

    Time: 307 ms (72.00%) | Memory: 104 MB (66.00%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+//Lets's Memoize this:
     int m, n;
+    int t[501][501][4][2]; //x, y, direction, turn
     vector<vector<int>> dir = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-    int DFS(int x,  int y, int d, bool turn, int currTarget,  vector<vector<int>> &grid){
+    int DFS(int x,  int y, int d, int turn, int currTarget,  vector<vector<int>> &grid){
         int newX = x + dir[d][0];
         int newY = y + dir[d][1];
 
-        if(newX >= m || newX < 0 || newY >= n || newY < 0 || grid[newX][newY] != currTarget){
+        if(newX >= m || newX < 0 || newY >= n || newY < 0 || grid[newX][newY] != currTarget || turn > 1){
             return 0;
         }
 
-        int best = DFS(newX, newY, d, turn, 2 - currTarget, grid);
+        if(t[newX][newY][d][turn] != -1) return t[newX][newY][d][turn];
 
-        if(turn){
-            best = max(best, DFS(newX, newY, (d + 1) % 4, false, 2 - currTarget, grid));
-        }
+        int notTakeTurn = 1 + DFS(newX, newY, d, turn, 2 - currTarget, grid);
+        int takeTurn = 1 + DFS(newX, newY, (d + 1) % 4, turn + 1, 2 - currTarget, grid);
 
-        return best + 1;
+        return t[newX][newY][d][turn] = max(notTakeTurn, takeTurn);
     }
 
     int lenOfVDiagonal(vector<vector<int>>& grid) {
@@ -24,11 +25,16 @@ public:
         n = grid[0].size();
         int result = 0;
 
+        // Intialize Table:
+        memset(t, -1, sizeof(t));
+
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(grid[i][j] == 1){
                     for(int d = 0; d<4; d++){
-                                            result = max(result, DFS(i, j, d, true, 2, grid) + 1);
+                        t[i][j][d][0] = 1;
+                        t[i][j][d][1] = 1;
+                        result = max(result, DFS(i, j, d, 0, 2, grid) + 1);
                     }
                 }
             }
