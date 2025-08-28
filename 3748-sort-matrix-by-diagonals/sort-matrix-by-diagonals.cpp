4eb4d71d89282988f08dc92db39a5e8db23528cb
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-29 01:01:19 +0530

    Time: 9 ms (60.49%) | Memory: 43.4 MB (70.78%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,31 +1,76 @@
 class Solution {
 public:
+    int m, n;
+    void sortDiagonal(int r, int c, vector<vector<int>> &grid, bool asc){
+        int i = r;
+        int j = c;
+
+        vector<int> temp;
+
+        while(i<m && j < n){
+            temp.push_back(grid[i++][j++]);
+        }
+
+        if(!asc){
+            sort(temp.begin(), temp.end(), greater<int>());
+        }else{
+            sort(temp.begin(), temp.end());
+        }
+
+        i = r;
+        j = c;
+        int k = 0;
+
+        while(i<m && j<n){
+            grid[i++][j++] = temp[k++];
+        }
+    };
+
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
         // Let's try out [i - j] approach:
-        int m = grid.size();
-        int n = grid[0].size();
-        unordered_map<int, vector<int>> mpp;
+        // int m = grid.size();
+        // int n = grid[0].size();
+        // unordered_map<int, vector<int>> mpp;
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                mpp[(i - j)].push_back(grid[i][j]);
-            }
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         mpp[(i - j)].push_back(grid[i][j]);
+        //     }
+        // }
+
+        // for(auto &it: mpp){
+        //     auto &vec = it.second;
+        //     if(it.first < 0){
+        //         sort(vec.rbegin(), vec.rend());
+        //     }else{
+        //         sort(vec.begin(), vec.end());
+        //     }
+        // }
+
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         grid[i][j] = mpp[(i-j)].back();
+        //          mpp[(i-j)].pop_back();
+        //     }
+        // }
+
+        // return grid;
+
+
+
+
+        // Lets try a different approach for resucing the space complexity(without taking the unordered_map):
+        m = grid.size();
+        n = grid[0].size();
+
+        // For Buttom-Left Diagonal:
+        for(int r = 0; r<m; r++){
+            sortDiagonal(r, 0, grid, false); // false is used for tracking the descending order and true for the ascending order
         }
 
-        for(auto &it: mpp){
-            auto &vec = it.second;
-            if(it.first < 0){
-                sort(vec.rbegin(), vec.rend());
-            }else{
-                sort(vec.begin(), vec.end());
-            }
-        }
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                grid[i][j] = mpp[(i-j)].back();
-                 mpp[(i-j)].pop_back();
-            }
+        //  For Upper-Right Diagonal:
+        for(int c = 1; c<n; c++){
+            sortDiagonal(0, c, grid, true);
         }
 
         return grid;
