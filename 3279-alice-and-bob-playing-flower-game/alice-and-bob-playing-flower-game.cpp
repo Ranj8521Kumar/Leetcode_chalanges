0cb3234683b1a3ca30d4932d04d906b3da6f66b7
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-30 02:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (50.21%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        // Brute Force Approach that may be give TLE:
+        // long long result = 0;
+        // for(int i = 1; i <= m; i++){
+        //     for(int j = 1; j <= n; j++){
+        //         if((i + j) % 2 != 0){
+        //             result++;
+        //         }
+        //     }
+        // }
+
+        // return result;
+
+        int o1 = (n % 2 == 0) ? n/2 : (n + 1)/2;
+        int e1 = (n % 2 == 0) ? n/2 : (n)/2;
+
+
+        int o2 = (m % 2 == 0) ? m/2 : (m + 1)/2;
+        int e2 = (m % 2 == 0) ? m/2 : (m)/2;
+
+        long long result = ((long long)o1 * (long long)e2) + ((long long)e1 * (long long)o2);
+
+        return result;
+    }
+};
