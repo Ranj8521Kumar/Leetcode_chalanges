264e5864ead2e865470565a4264ac58c8fe11030
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-30 04:06:43 +0530

    Time: 422 ms (5.01%) | Memory: 61.1 MB (6.88%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int m, n;
+    int solve(int r1, int r2, vector<vector<int>>& mat){
+        vector<int> temp(n, 0);
+        
+        for(int j = 0; j < n; j++){
+            bool isZero = false;
+            for(int i = r1; i <= r2; i++){
+                if(mat[i][j] == 0){
+                    isZero = true;
+                    break;
+                }
+            }
+
+            if(!isZero){
+                temp[j] = (j == 0) ? 1 : temp[j-1] + 1;
+            }
+        }
+
+        int result = 0;
+        for(auto &it: temp){
+            if(it != 0){
+                result += it;
+            }
+        }
+
+        return result;
+    }
+
+
+    int numSubmat(vector<vector<int>>& mat) {
+        m = mat.size();
+        n = mat[0].size();
+        int result = 0;
+
+        for(int r1 = 0; r1 < m; r1++){
+            for(int r2 = r1; r2 < m; r2++){
+                result += solve(r1, r2, mat);
+            }
+        }
+
+        return result;
+    }
+};
