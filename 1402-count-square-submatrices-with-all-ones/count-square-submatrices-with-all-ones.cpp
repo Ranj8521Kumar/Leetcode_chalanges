f9ff11299d09e01c4e8fb0cf1579b6e899a3d231
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-30 05:45:52 +0530

    Time: 11 ms (32.22%) | Memory: 30.4 MB (38.10%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -110,12 +110,6 @@ public:
 
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                t[i][j] = matrix[i][j];
-            }
-        }
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
                 if(i == 0 || j == 0){
                     t[i][j] = matrix[i][j];
                 }else if(matrix[i][j] == 1){
