baeaa271d7fcd25814e86e9a59c66c0ed4d2586f
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-31 03:38:46 +0530

    Time: 9 ms (58.27%) | Memory: 31.4 MB (9.11%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -34,17 +34,39 @@ public:
 
         // Only recursion throw the TLE, so lets memoize it:
 
+        // m = matrix.size();
+        // n = matrix[0].size();
+        // int maxLength = 0;
+
+        // memset(t, -1, sizeof(t));
+
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(matrix[i][j] == '1'){
+        //             maxLength = max(maxLength, solve(i, j, matrix));
+        //         }
+        //     }
+        // }
+
+        // return maxLength*maxLength;
+
+
+        // Done, but let's try out using buttom-up approach:
         m = matrix.size();
         n = matrix[0].size();
         int maxLength = 0;
 
-        memset(t, -1, sizeof(t));
+        vector<vector<int>> dp(m, vector<int> (n, 0));
 
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(matrix[i][j] == '1'){
-                    maxLength = max(maxLength, solve(i, j, matrix));
+                if(i == 0 || j == 0){
+                    dp[i][j] = (matrix[i][j] == '1') ? 1 : 0;
+                }else if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
                 }
+
+                maxLength = max(maxLength, dp[i][j]);
             }
         }
 
