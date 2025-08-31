6e383fc18c253a6544f0a41d78d59a38f749dc9d
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-01 05:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (6.43%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result;
+        // vector<int> result;
+        // int carry = 0;
+        // int n = digits.size();
+
+        // result.push_back((digits[n-1] + 1) % 10);
+        // carry = (digits[n-1] + 1) / 10;
+
+        // for(int i = n-2; i>=0; i--){
+        //     result.push_back((digits[i] + carry) % 10);
+        //     carry = (digits[i] + carry) / 10;
+        // }
+
+        // if(carry != 0){
+        //     result.push_back(carry);
+        // }
+
+        // reverse(result.begin(), result.end());
+
+        // return result;
+
+
+        
+
+        // Without taking an extra space:
         int carry = 0;
         int n = digits.size();
 
-        result.push_back((digits[n-1] + 1) % 10);
-        carry = (digits[n-1] + 1) / 10;
+        for(int i = n-1; i>=0; i--){
+            if(digits[i] < 9){
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
 
-        for(int i = n-2; i>=0; i--){
-            result.push_back((digits[i] + carry) % 10);
-            carry = (digits[i] + carry) / 10;
+            digits[i] = 0;
+            carry = 1;
         }
 
         if(carry != 0){
-            result.push_back(carry);
+            digits.insert(digits.begin(), carry);
         }
 
-        reverse(result.begin(), result.end());
-
-        return result;
+        return digits;
     }
 };
