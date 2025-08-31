ae4e479c9eb50fc3ab7f9691d9a5e99d7721e4e5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-08-31 07:30:07 +0530

    Time: 23 ms (55.72%) | Memory: 17.4 MB (56.70%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-
         vector<vector<int>> result;
+
         sort(nums.begin(), nums.end());
 
         for(int i = 0; i<n; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            for(int j = i + 1; j<n; j++){
-                if(j > i+1){
-                    if(nums[j] == nums[j-1]){
-                        continue;
-                    }
-                }
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            long long sum = 0;
+            
+            for(int j = i+1; j<n; j++){
+                int k = j + 1;
+                int l = n - 1;
 
-                int k = j+1;
-                int l = n-1;
+                 if((j - i) > 1 && j < k && nums[j] == nums[j-1]) continue;
 
                 while(k < l){
-                    long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[k] + (long long)nums[l];
-                    if(sum > target){
-                        l--;
-                    }else if(sum < target){
-                        k++;
-                    }else{
+                    sum = 1LL*nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
                         result.push_back({nums[i], nums[j], nums[k], nums[l]});
                         k++;
                         l--;
@@ -38,6 +30,10 @@ public:
                         while(k < l && nums[l] == nums[l+1]){
                             l--;
                         }
+                    }else if(sum > target){
+                        l--;
+                    }else{
+                        k++;
                     }
                 }
             }
