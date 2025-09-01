57ffd3818e56ad94d7272a57e469b85d900a883a
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-02 02:11:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.49%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    int t[20];
     int catalan(int n){
         if(n <= 1) return 1;
+
+        if(t[n] != -1) return t[n];
 
         int res = 0;
         for(int i = 0; i<n; i++){
             res += catalan(i) * catalan(n - i - 1);
         }
 
-        return res;
+        return t[n] = res;
 
     }
 
     int numTrees(int n) {
+        memset(t, -1, sizeof(t));
+
         return catalan(n);
     }
 };
