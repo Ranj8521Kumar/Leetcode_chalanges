65e018ce8544162c0aa6aae6bdf47416b548d6a4
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-02 01:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (57.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
+        // int n = nums.size();
         
+        // int prod = 1;
+        // vector<int> prefix(n, 1);
+        // for(int i = 1; i<n; i++){
+        //     prod = prod * nums[i-1];
+        //     prefix[i] =  prod;
+        // }
+
+        // prod = 1;
+        // vector<int> suffix(n, 1);
+        // for(int  i = n-2; i>=0; i--){
+        //     prod = prod * nums[i+1];
+        //     suffix[i] =  prod;
+        // }
+
+        // for(int i = 0; i<n; i++){
+        //     nums[i] = prefix[i]*suffix[i];
+        // }
+
+        // return nums;
+
+
+        // Let's try to solve this question in  O(1) space complexity, according to the question:
+        int n = nums.size();
         int prod = 1;
-        vector<int> prefix(n, 1);
-        for(int i = 1; i<n; i++){
-            prod = prod * nums[i-1];
-            prefix[i] =  prod;
+        vector<int> result(n, 1);
+        
+        for(int i = 0; i<n; i++){
+            result[i] = prod;
+            prod *= nums[i];
         }
 
         prod = 1;
-        vector<int> suffix(n, 1);
-        for(int  i = n-2; i>=0; i--){
-            prod = prod * nums[i+1];
-            suffix[i] =  prod;
+        for(int i = n-1; i>=0; i--){
+            result[i] = result[i] * prod;
+            prod = prod * nums[i];
+
         }
 
-        for(int i = 0; i<n; i++){
-            nums[i] = prefix[i]*suffix[i];
-        }
-
-        return nums;
+        return result;
     }
 };
