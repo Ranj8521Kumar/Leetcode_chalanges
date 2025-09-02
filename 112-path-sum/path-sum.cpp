84967d4737533857d5f5f3e383ea2f82ff9e7336
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-03 03:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (68.27%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,19 +11,19 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* node, int targetSum, int &sum){
+    bool solve(TreeNode* node, int targetSum, int sum){
 
         if(node == nullptr) return false;
 
-        sum = sum + node->val; // Add current node value
+        sum = sum + node->val; // Add current node value(do)
 
         if(!node->left && !node->right){
             if(targetSum == sum) return true;
         }
 
-        bool found = (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum));
+        bool found = (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
         
-        sum -= node->val; // Backtrack
+        sum -= node->val; // Backtrack(undo)
         return found;
     }
 
@@ -31,4 +31,29 @@ public:
         int sum = 0;
         return solve(root, targetSum, sum);
     }
+
+
+
+    // Another Approach:
+    // bool solve(TreeNode* node, int targetSum, int &sum){
+
+    //     if(node == nullptr) return false;
+
+    //     sum = sum + node->val; // Add current node value(do)
+
+    //     if(!node->left && !node->right){
+    //         if(targetSum == sum) return true;
+    //     }
+
+    //     bool found = (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
+        
+    //     sum -= node->val; // Backtrack(undo)
+    //     return found;
+    // }
+
+    // bool hasPathSum(TreeNode* root, int targetSum) {
+    //     int sum = 0;
+    //     return solve(root, targetSum, sum);
+    // }
+    
 };
