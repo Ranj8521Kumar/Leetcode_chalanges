e80f45d61551266bfc7ee580675f0b2f6eaed431
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-02 17:01:03 +0530

    Time: 79 ms (13.33%) | Memory: 55 MB (6.67%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -38,7 +38,51 @@ public:
         // return count;
 
 
-        // Let's Optimize:
+        // // Let's Optimize:
+        // int n = points.size();
+        // int count = 0;
+        // sort(points.begin(), points.end(), [&](vector<int> &a, vector<int> &b){
+        //     if(a[0] == b[0]){
+        //         return a[1] >= b[1];
+        //     }
+
+        //     return a[0] <= b[0];
+        // });
+
+        // for(int i = 0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         auto point1 = points[i];
+        //         auto point2 = points[j];
+
+        //         int x = point1[0];
+        //         int y = point1[1];
+
+        //         int nx = point2[0];
+        //         int ny = point2[1];
+
+        //         if(ny <= y){
+        //             bool inside = false;
+        //             for(int k = i+1; k<j; k++){
+        //                 auto point3 = points[k];
+        //                 int x1 = point3[0];
+        //                 int y1 = point3[1];
+
+        //                 if((y1 >= ny && y1 <= y)){
+        //                     inside = true;
+        //                 }
+        //             }
+
+        //             if(inside == false) count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+
+
+
+        // Let's Optimize to O(n^2):
         int n = points.size();
         int count = 0;
         sort(points.begin(), points.end(), [&](vector<int> &a, vector<int> &b){
@@ -50,6 +94,7 @@ public:
         });
 
         for(int i = 0; i<n; i++){
+            int prev = INT_MIN;
             for(int j = i+1; j<n; j++){
                 auto point1 = points[i];
                 auto point2 = points[j];
@@ -61,18 +106,12 @@ public:
                 int ny = point2[1];
 
                 if(ny <= y){
-                    bool inside = false;
-                    for(int k = i+1; k<j; k++){
-                        auto point3 = points[k];
-                        int x1 = point3[0];
-                        int y1 = point3[1];
-
-                        if((y1 >= ny && y1 <= y)){
-                            inside = true;
-                        }
+                    if((prev >= ny && prev <= y)){
+                        continue;
                     }
 
-                    if(inside == false) count++;
+                    count++;
+                    prev = ny;
                 }
             }
         }
