e3ad77f45976c568c06f68789281462a1c934650
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-02 16:26:16 +0530

    Time: 427 ms (5.19%) | Memory: 134.9 MB (5.19%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,11 +1,56 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
+        // Brute Force Approach
+        // int n = points.size();
+        // int count = 0;
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(i == j) continue;
+        //         auto point1 = points[i];
+        //         auto point2 = points[j];
+
+        //         int x = point1[0];
+        //         int y = point1[1];
+
+        //         int nx = point2[0];
+        //         int ny = point2[1];
+
+        //         if(nx >= x && ny <= y){
+        //             bool inside = false;
+        //             for(int k = 0; k<n; k++){
+        //                 if(k == i || k == j) continue;
+
+        //                 auto point3 = points[k];
+        //                 int x1 = point3[0];
+        //                 int y1 = point3[1];
+
+        //                 if((x1 >= x && x1 <= nx) && (y1 >= ny && y1 <= y)){
+        //                     inside = true;
+        //                 }
+        //             }
+
+        //             if(inside == false) count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+
+
         int n = points.size();
         int count = 0;
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b){
+            if(a[0] == b[0]){
+                return a[1] >= b[1];
+            }
+
+            return a[0] <= b[0];
+        });
+        
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(i == j) continue;
+            for(int j = i+1; j<n; j++){
                 auto point1 = points[i];
                 auto point2 = points[j];
 
@@ -15,16 +60,14 @@ public:
                 int nx = point2[0];
                 int ny = point2[1];
 
-                if(nx >= x && ny <= y){
+                if(ny <= y){
                     bool inside = false;
-                    for(int k = 0; k<n; k++){
-                        if(k == i || k == j) continue;
-
+                    for(int k = i+1; k<j; k++){
                         auto point3 = points[k];
                         int x1 = point3[0];
                         int y1 = point3[1];
 
-                        if((x1 >= x && x1 <= nx) && (y1 >= ny && y1 <= y)){
+                        if((y1 >= ny && y1 <= y)){
                             inside = true;
                         }
                     }
