954393e92da6e6cf8f2811c61957d7b384488b9d
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-02 18:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (20.44%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -10,23 +10,53 @@
  */
 class Solution {
 public:
+    void solve(ListNode* prev){
+        if(!prev->next || !prev->next->next){
+            return;
+        }
+
+        ListNode* first = prev->next;
+        ListNode* second = first->next;
+
+        //swapping
+        first->next = second->next;
+        second->next = first;
+        prev->next = second;
+
+        prev = first;
+
+        solve(prev);
+    }
+
     ListNode* swapPairs(ListNode* head) {
+        // ListNode* dummy = new ListNode(-1);
+        // dummy->next = head;
+
+        // ListNode* prev = dummy;
+
+        // while(prev->next && prev->next->next){
+        //     ListNode* first = prev->next;
+        //     ListNode* second = first->next;
+
+        //     // Swapping
+        //     first->next =second->next;
+        //     second->next = first;
+        //     prev->next = second;
+
+        //     prev = first;
+        // }
+
+        // return dummy->next;
+
+
+
+
+        // Recursion Approach:
         ListNode* dummy = new ListNode(-1);
         dummy->next = head;
-
         ListNode* prev = dummy;
-
-        while(prev->next && prev->next->next){
-            ListNode* first = prev->next;
-            ListNode* second = first->next;
-
-            // Swapping
-            first->next =second->next;
-            second->next = first;
-            prev->next = second;
-
-            prev = first;
-        }
+        
+        solve(prev);
 
         return dummy->next;
     }
