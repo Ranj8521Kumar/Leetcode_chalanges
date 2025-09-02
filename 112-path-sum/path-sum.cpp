b4e6a4356a26eb18071643d5c701cb70a1db0390
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-03 04:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (5.70%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,6 +11,31 @@
  */
 class Solution {
 public:
+    // First Approach(sum is passed by Reference, so need to rescusive undo/backtrack, for each recursive call, it has It's changes to original value of sum, so, when the function calls end, we have to explicitly undo/backtrack)
+    bool solve(TreeNode* node, int targetSum, int &sum){
+
+        if(node == nullptr) return false;
+
+        sum = sum + node->val; // Add current node value(do)
+
+        if(!node->left && !node->right){
+            if(targetSum == sum) return true;
+        }
+
+        bool found = (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
+        
+        sum -= node->val; // Backtrack(undo)
+        return found;
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum = 0;
+        return solve(root, targetSum, sum);
+    }
+
+
+
+    // Another Approach(sum is passed by value, so no need to rescusive undo/backtrack, for each recursive call, it has It's own local copy, that resets automatically, when the function calls end):
     // bool solve(TreeNode* node, int targetSum, int sum){
 
     //     if(node == nullptr) return false;
@@ -21,35 +46,11 @@ public:
     //         if(targetSum == sum) return true;
     //     }
 
-    //     bool found = (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
-        
-    //     sum -= node->val; // Backtrack(undo)
-    //     return found;
+    //     return (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
     // }
 
     // bool hasPathSum(TreeNode* root, int targetSum) {
     //     int sum = 0;
     //     return solve(root, targetSum, sum);
-    // }
-
-
-
-    // Another Approach:
-    bool solve(TreeNode* node, int targetSum, int sum){
-
-        if(node == nullptr) return false;
-
-        sum = sum + node->val; // Add current node value(do)
-
-        if(!node->left && !node->right){
-            if(targetSum == sum) return true;
-        }
-
-        return (solve(node->left, targetSum, sum) || solve(node->right, targetSum, sum)); // explore
-    }
-
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        return solve(root, targetSum, 0);
-    }
-    
+    // } 
 };
