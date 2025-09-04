23104803e45e7cc558decf70674820fe9e7e7dd5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-04 07:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (31.11%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,41 +11,69 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+    //     if(!head || !head->next) return head;
+
+    //     int n = 0;
+    //     ListNode* curr = head;
+    //     while(curr){
+    //         n++;
+    //         curr = curr->next;
+    //     }
+
+    //     curr = head;
+    //     ListNode* dummyHead = head;
+    //     k = k % n;
+    //     int cnt = n - k - 1;
+    //     while(cnt > 0){
+    //         curr = curr->next;
+    //         cnt--;
+    //     }
+
+    //     bool first = true;
+    //     while(curr->next){
+    //         if(first){
+    //             ListNode* temp = curr->next;
+    //             curr->next = curr->next->next;
+    //             temp->next = head;
+    //             head = temp;
+    //             dummyHead = temp;
+    //             first = false;
+    //         }else{
+    //             ListNode* temp = curr->next;
+    //             curr->next = curr->next->next;
+    //             ListNode* temp1 = dummyHead->next;
+    //             dummyHead->next = temp;
+    //             dummyHead = dummyHead->next;
+    //             dummyHead->next = temp1;
+    //         }
+    //     }
+
+    //     return head;
+    // }
+
+
+
+
+    // Another Approach:
         if(!head || !head->next) return head;
 
         int n = 0;
         ListNode* curr = head;
-        while(curr){
+        while(curr->next){
             n++;
             curr = curr->next;
         }
+        n++;
 
-        curr = head;
-        ListNode* dummyHead = head;
         k = k % n;
-        int cnt = n - k - 1;
+        int cnt = n - k;
         while(cnt > 0){
+            curr->next = head;
+            head = head->next;
             curr = curr->next;
-            cnt--;
-        }
+            curr->next = nullptr;
 
-        bool first = true;
-        while(curr->next){
-            if(first){
-                ListNode* temp = curr->next;
-                curr->next = curr->next->next;
-                temp->next = head;
-                head = temp;
-                dummyHead = temp;
-                first = false;
-            }else{
-                ListNode* temp = curr->next;
-                curr->next = curr->next->next;
-                ListNode* temp1 = dummyHead->next;
-                dummyHead->next = temp;
-                dummyHead = dummyHead->next;
-                dummyHead->next = temp1;
-            }
+            cnt--;
         }
 
         return head;
