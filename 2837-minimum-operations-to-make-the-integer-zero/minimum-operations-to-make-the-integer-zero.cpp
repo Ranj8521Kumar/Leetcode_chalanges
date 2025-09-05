dc55a850fa0c6aa89907b78d07db00eeb1b91b91
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-05 16:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (52.07%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        int k = 1;
+    //     int k = 1;
 
-        while(true){
+    //     while(true){
+    //         long long val = (long long)num1 - (long long)k * num2;
+    //         if(val <= 0) return -1;
+
+    //         int setBits = __builtin_popcountll(val);
+    //         if(setBits <= k && k <= val){
+    //             return k;
+    //         }
+
+    //         k++;
+    //     }
+
+    //     return -1;
+    // }
+
+
+    // Another Approach:
+    int k = 1;
+
+        for(int k = 1; k<=35; k++){
             long long val = (long long)num1 - (long long)k * num2;
             if(val <= 0) return -1;
 
@@ -11,8 +30,6 @@ public:
             if(setBits <= k && k <= val){
                 return k;
             }
-
-            k++;
         }
 
         return -1;
