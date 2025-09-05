4f1825fd1a27ebd3332061f9c48bb5f03e22e46e
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-05 17:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.11%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flips = 0;
+        // Brute Force Approach
+        // int flips = 0;
 
-        while( a != 0 ||  b != 0 || c != 0){
-            int rightMostBitOfa = (a & 1);
-            int rightMostBitOfb = (b & 1);
-            int rightMostBitOfc = (c & 1);
+        // while( a != 0 ||  b != 0 || c != 0){
+        //     int rightMostBitOfa = (a & 1);
+        //     int rightMostBitOfb = (b & 1);
+        //     int rightMostBitOfc = (c & 1);
 
-            if(rightMostBitOfc == 1){
-                if(rightMostBitOfa == 0 && rightMostBitOfb == 0){
-                    flips++;
-                }
-            }else if(rightMostBitOfc == 0){
-                if(rightMostBitOfa == 1 && rightMostBitOfb == 1){
-                    flips += 2;
-                }else if(rightMostBitOfa == 1 || rightMostBitOfb == 1){
-                    flips++;
-                }
-            }
+        //     if(rightMostBitOfc == 1){
+        //         if(rightMostBitOfa == 0 && rightMostBitOfb == 0){
+        //             flips++;
+        //         }
+        //     }else if(rightMostBitOfc == 0){
+        //         if(rightMostBitOfa == 1 && rightMostBitOfb == 1){
+        //             flips += 2;
+        //         }else if(rightMostBitOfa == 1 || rightMostBitOfb == 1){
+        //             flips++;
+        //         }
+        //     }
 
-            // Right Shift of a, b and c
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
+        //     // Right Shift of a, b and c
+        //     a >>= 1;
+        //     b >>= 1;
+        //     c >>= 1;
+        // }
 
-        return flips;
+        // return flips;
+
+
+
+        // Another Approach:
+        int result = (a|b)^c;
+        int result1 = (a & b) & result;
+
+        return __builtin_popcount(result) + __builtin_popcount(result1);
     }
 };
