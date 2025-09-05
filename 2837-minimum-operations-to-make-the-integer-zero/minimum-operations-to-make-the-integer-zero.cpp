62c251c63ae045743b343b349818448631e875e1
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-05 16:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (52.07%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-    //     int k = 1;
+    //     int k = 0;
 
     //     while(true){
     //         long long val = (long long)num1 - (long long)k * num2;
@@ -20,9 +20,25 @@ public:
 
 
     // Another Approach:
-    int k = 1;
 
-        for(int k = 1; k<=35; k++){
+    //     for(int k = 0; k<=35; k++){
+    //         long long val = (long long)num1 - (long long)k * num2;
+    //         if(val <= 0) return -1;
+
+    //         int setBits = __builtin_popcountll(val);
+    //         if(setBits <= k && k <= val){
+    //             return k;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+
+
+    // Or set limit for k is according to the constraint:
+
+        for(int k = 0; k<=60; k++){
             long long val = (long long)num1 - (long long)k * num2;
             if(val <= 0) return -1;
 
