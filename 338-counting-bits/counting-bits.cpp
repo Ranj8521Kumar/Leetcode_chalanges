12ef3c8cc76ac43965f7658573e3122f2a3631b5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-07 00:47:37 +0530

    Time: 2 ms (34.99%) | Memory: 11.9 MB (9.26%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    int t[100001];
+    int countOnes(int i){
+        if(i == 0) return 0;
+
+        if(t[i] != -1) return t[i];
+
+        return t[i] = countOnes(i/2) + i%2;
+    }
+
+
     vector<int> countBits(int n) {
         // vector<int> result;
         
@@ -31,17 +41,27 @@ public:
     // }
 
 
-    // DP Approach(O(n)) Complexity:
+    // // DP Approach(O(n)) Complexity:
+        // vector<int> resultDP(n+1, 0);
 
-        if(n == 0) return {0};
-        vector<int> resultDP(n+1, 0);
-        resultDP[0] = 0;
-        resultDP[1] = 1;
+        // for(int i = 1; i<=n; i++){
+        //     resultDP[i] = resultDP[i/2] + (i % 2);
+        // }
 
-        for(int i = 2; i<=n; i++){
-            resultDP[i] = resultDP[i/2] + (i % 2);
+        // return resultDP;
+
+
+
+    
+
+    // Recursive Approach:
+        vector<int> result;
+        memset(t, -1, sizeof(t));
+
+        for(int i = 0; i<=n; i++){
+            result.push_back(countOnes(i));
         }
 
-        return resultDP;
+        return result;
     }
 };
