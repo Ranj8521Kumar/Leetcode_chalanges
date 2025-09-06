83123042e69397490a9b2a27970bdb26b3ac44e7
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-06 09:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (35.13%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,7 +1,42 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xorSum = 0;
+    //     int xorSum = 0;
+
+    //     for(auto &num: nums){
+    //         xorSum ^= num;
+    //     } // xorSum contains only those numbers that appears only single times
+
+    //     // Calculate Mask(number with first rightmost set bit of XOR is one)
+    //     int mask;
+    //     for(int k = 0; k<=31; k++){
+    //         if(xorSum & 1){//get rightmost bit of xorSum
+    //             mask = (1 << k);//create mask, where rightMost setBit of xorSum
+    //             break;
+    //         }
+    //         xorSum >>= 1;
+    //     }
+
+    //     // Divide whole nums in two groups, such that both  numbers that appears exactly one is come under one another group
+    //     int a = 0;
+    //     int b = 0;
+    //     for(auto &num: nums){
+    //         if(num & mask){
+    //             a ^= num;
+    //         }else{
+    //             b ^= num;
+    //         }
+    //     }
+
+    //     return {a,  b};
+    // }
+
+
+
+
+
+    // We can directly find the setRightMost bit of xorSum:
+        long long xorSum = 0;
 
         for(auto &num: nums){
             xorSum ^= num;
@@ -9,13 +44,16 @@ public:
 
         // Calculate Mask(number with first rightmost set bit of XOR is one)
         int mask;
-        for(int k = 0; k<=31; k++){
-            if(xorSum & 1){//get rightmost bit of xorSum
-                mask = (1 << k);//create mask, where rightMost setBit of xorSum
-                break;
-            }
-            xorSum >>= 1;
-        }
+        // for(int k = 0; k<=31; k++){
+        //     if(xorSum & 1){//get rightmost bit of xorSum
+        //         mask = (1 << k);//create mask, where rightMost setBit of xorSum
+        //         break;
+        //     }
+        //     xorSum >>= 1;
+        // }
+
+
+        mask = (xorSum & (-xorSum)); // Get number with first rightmost set bit of a number
 
         // Divide whole nums in two groups, such that both  numbers that appears exactly one is come under one another group
         int a = 0;
