7a93923454dd1fc8d76842f7b0906d72b5423e43
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-07 00:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.85%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -10,23 +10,38 @@ public:
         // return result;
 
 
-        // Another Approach:
-        vector<int> result;
+    //     // Another Approach:
+    //     vector<int> result;
 
-        for(int i = 0; i<=n; i++){
-            int countOne = 0;
-            int num = i;
-            while(num != 0){
-                if((num&1) == 1){
-                    countOne++;
-                }
+    //     for(int i = 0; i<=n; i++){
+    //         int countOne = 0;
+    //         int num = i;
+    //         while(num != 0){
+    //             if((num&1) == 1){
+    //                 countOne++;
+    //             }
 
-                num >>= 1;
-            }
+    //             num >>= 1;
+    //         }
 
-            result.push_back(countOne);
+    //         result.push_back(countOne);
+    //     }
+
+    //     return result;
+    // }
+
+
+    // DP Approach(O(n)) Complexity:
+
+        if(n == 0) return {0};
+        vector<int> resultDP(n+1, 0);
+        resultDP[0] = 0;
+        resultDP[1] = 1;
+
+        for(int i = 2; i<=n; i++){
+            resultDP[i] = resultDP[i/2] + (i % 2);
         }
 
-        return result;
+        return resultDP;
     }
 };
