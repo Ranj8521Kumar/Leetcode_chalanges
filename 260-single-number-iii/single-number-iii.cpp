a35117055e87e66fcb251d13f91bcf1cbd611303
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-06 09:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (82.33%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xorSum = 0;
+
+        for(auto &num: nums){
+            xorSum ^= num;
+        } // xorSum contains only those numbers that appears only single times
+
+        // Calculate Mask(number with first rightmost set bit of XOR is one)
+        int mask;
+        for(int k = 0; k<=31; k++){
+            if(xorSum & 1){//get rightmost bit of xorSum
+                mask = (1 << k);//create mask, where rightMost setBit of xorSum
+                break;
+            }
+            xorSum >>= 1;
+        }
+
+        // Divide whole nums in two groups, such that both  numbers that appears exactly one is come under one another group
+        int a = 0;
+        int b = 0;
+        for(auto &num: nums){
+            if(num & mask){
+                a ^= num;
+            }else{
+                b ^= num;
+            }
+        }
+
+        return {a,  b};
+    }
+};
