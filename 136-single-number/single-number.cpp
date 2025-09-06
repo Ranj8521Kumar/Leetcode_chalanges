122076d3706df5d8df27aa2fb6b4f4a8c91ce769
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-06 08:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.16%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // //Generalised Approach to solving this type of Problem
+        // int result = 0;
+        // for(int k = 0; k<=31; k++){
+        //     int countOnes = 0;
+        //     int mask = (1<<k);
+
+        //     for(auto &num: nums){
+        //         if((num & mask) != 0){//check  kth bit of nums is one or not
+        //             countOnes += 1;
+        //         }
+        //     }
+
+        //     if(countOnes % 2 == 1){
+        //         result = (result | (mask)); // set kth bit of resut to one
+        //     }
+        // }
+
+        // return result;
+
         int result = 0;
-        for(int k = 0; k<=31; k++){
-            int countOnes = 0;
-            int mask = (1<<k);
-
-            for(auto &num: nums){
-                if((num & mask) != 0){//check  kth bit of nums is one or not
-                    countOnes += 1;
-                }
-            }
-
-            if(countOnes % 2 == 1){
-                result = (result | (mask)); // set kth bit of resut to one
-            }
+        for(auto &num: nums){
+            result ^= num;
         }
 
         return result;
