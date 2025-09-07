88e2f506425052aead7abc55920540748228e801
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-07 19:36:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (13.58%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -4,14 +4,15 @@ public:
         vector<int> result;
 
         if(n % 2 == 0){
-            for(int i = -(n/2); i<=(n/2); i++){
-                if(i == 0) continue;
-
+            for(int i = 1; i<=(n/2); i++){
                 result.push_back(i);
+                result.push_back(-i);
             }
         }else{
-            for(int i = -(n/2); i<=(n/2); i++){
+            result.push_back(0);
+            for(int i = 1; i<=(n/2); i++){
                 result.push_back(i);
+                result.push_back(-i);
             }
         }
 
