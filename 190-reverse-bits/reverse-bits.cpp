82260bf724193ee12bc998878f1ffdb7904682b7
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-07 19:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int reverseBits(int n) {
+        // int result = 0;
+
+        // for(int k = 0; k<=31; k++){
+        //     if((n & 1) == 1){
+        //         result += (1 << (31 - k));
+        //     }
+
+        //     n >>= 1;
+        // }
+
+        // return result;
+
+
+
+
+
+        // Another Method:
         int result = 0;
 
         for(int k = 0; k<=31; k++){
-            if((n & 1) == 1){
-                result += (1 << (31 - k));
-            }
-
+            result <<= 1;
+            result |= (n & 1);
             n >>= 1;
         }
 
