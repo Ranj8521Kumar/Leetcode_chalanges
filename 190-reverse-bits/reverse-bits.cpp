711caa28660bc7d4390779bd1255d41e5b18fe51
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-07 20:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -18,7 +18,35 @@ public:
 
 
         // Another Method:
-        int result = 0;
+        // int result = 0;
+
+        // for(int k = 0; k<=31; k++){
+        //     result <<= 1;
+        //     result |= (n & 1);
+        //     n >>= 1;
+        // }
+
+        // return result;
+
+
+
+//  Why unsigned int?
+
+
+/* Bitwise operations don’t care about sign
+        But if you use int result, the sign bit (MSB) could     cause confusion when shifting.
+        Example: If result becomes something like 1000...000 in binary, an int would treat it as negative, while unsigned int keeps it as a large positive number. */
+
+/*Left shifts (<<) are safer with unsigned:
+    In C++, left-shifting a signed integer into the sign bit is undefined behavior.
+    With unsigned int, shifting is always well-defined because it just rotates bits and doesn’t care about sign.*/
+
+/*We only want raw bit patterns:
+    When reversing bits, we don’t actually want to interpret them as negative/positive until the very end.
+    Using unsigned int means result is just a bucket of 32 raw bits.
+    At the end, you cast back to int, so the caller gets the correct signed interpretation.*/
+
+        unsigned int result = 0;
 
         for(int k = 0; k<=31; k++){
             result <<= 1;
@@ -26,6 +54,6 @@ public:
             n >>= 1;
         }
 
-        return result;
+        return (int)result;
     }
 };
