aeb51c9e4cd6c476968b6c24b82e994a3f1f5550
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-08 15:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.14%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -13,11 +13,26 @@ public:
         return true;
     }
 
-    vector<int> getNoZeroIntegers(int n) {
-        for(int i = 1; i<=n/2; i++){
-            if(noZero(i) && noZero(n - i)){
-                return {i, n-i};
+    // vector<int> getNoZeroIntegers(int n) { // nlog(n) Complexity
+    //     for(int i = 1; i<=n/2; i++){
+    //         if(noZero(i) && noZero(n - i)){
+    //             return {i, n-i};
+    //         }
+    //     }
+
+    //     return {};
+    // }
+
+
+
+    vector<int> getNoZeroIntegers(int n) { // log(n) Complexity
+        int a = 1; int b = n-1;
+        while(true){
+            if(noZero(a) && noZero(b)){
+                return {a, b};
             }
+            a++;
+            b--;
         }
 
         return {};
