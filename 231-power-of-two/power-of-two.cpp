b411d9fd1131de8b67e5d840e3c5048da5fb8953
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-08 16:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.59%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i = 0; i<=log2(n); i++){
-            if(pow(2, i) == n){
+        // for(int i = 0; i<=log2(n); i++){
+        //     if(pow(2, i) == n){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+
+
+
+
+        // Second Approach:
+        if(n <= 0) return false;
+
+        for(int k = 0;  k<=31; k++){
+            if((n ^ (1 << k)) == 0){
                 return true;
             }
         }
