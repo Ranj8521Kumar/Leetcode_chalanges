8eebff2b7c2a817eb1cd08b5d49148adec0e6f33
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-09 19:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (52.52%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> mps;
-        unordered_map<char, int> mpt;
+        // unordered_map<char, int> mps;
+        // unordered_map<char, int> mpt;
 
-        for(auto &ch: t){
-            mpt[ch]++;
-        }
+        // for(auto &ch: t){
+        //     mpt[ch]++;
+        // }
 
+        // for(auto &ch: s){
+        //     mps[ch]++;
+        // }
+
+        // for(auto &ch: t){
+        //     if(mpt[ch] > mps[ch]){
+        //         return ch;
+        //     }
+        // }
+
+        // return ' ';
+
+
+
+
+
+
+
+        //Another Method:
+        char result = ' ';
         for(auto &ch: s){
-            mps[ch]++;
+            result ^= ch;
         }
 
         for(auto &ch: t){
-            if(mpt[ch] > mps[ch]){
-                return ch;
-            }
+            result ^= ch;
         }
 
-        return ' ';
+        return tolower(result);
     }
 };
