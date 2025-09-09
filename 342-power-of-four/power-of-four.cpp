a942dd77e126840544cc729ad257f01406e2b3e8
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-09 11:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.97%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -13,9 +13,22 @@ public:
 
 
         // Another Method:
-        if(n <= 0) return false;
-        for(int k = 0; k<=31; k+=2){
-            if((1<<k) == n){
+        // if(n <= 0) return false;
+        // for(int k = 0; k<=31; k+=2){
+        //     if((1<<k) == n){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+
+
+
+        // Third Approach:
+        if(n <= 0) return 0;
+        if((n & (n - 1)) == 0){ // Power of 2
+            if((n & (0x555555555)) != 0){ // power of 4(power of 2 has set bit at even position);
                 return true;
             }
         }
