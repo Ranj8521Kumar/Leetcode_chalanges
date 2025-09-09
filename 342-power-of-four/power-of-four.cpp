419330fc27931d2d5987efa95f1d0a619c5b6f9b
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-09 11:15:28 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (18.82%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        for(int i = 0; i<=(log2(n)/2); i++){
-            if(pow(4, i) == n){
+        // for(int i = 0; i<=(log2(n)/2); i++){
+        //     if(pow(4, i) == n){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+
+
+
+        // Another Method:
+        if(n <= 0) return false;
+        for(int k = 0; k<=31; k+=2){
+            if((1<<k) == n){
                 return true;
             }
         }
