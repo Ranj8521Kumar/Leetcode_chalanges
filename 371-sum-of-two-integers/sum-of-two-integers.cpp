ff8762b73088de516ec6f1afe149fa6967a2dbef
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-09 20:40:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.30%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        // int result = 0;
+        // int carry = 0;
+        // for(int k = 0; k<=31; k++){
+        //     int ra = (a & 1);
+        //     int rb = (b & 1);
+
+        //     if(carry == 0){
+        //         if(ra == 0 && rb == 0){
+                   
+        //         }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
+        //             result |= (1 << k);
+        //         }else{
+        //             carry = 1;
+        //         }
+        //     }else{
+        //         if(ra == 0 && rb == 0){
+        //            result |= (1 << k);
+        //            carry = 0;
+        //         }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
+        //             carry = 1;
+        //         }else{
+        //             result |= (1 << k);
+        //             carry = 1;
+        //         }
+        //     }
+
+        //     a >>= 1;
+        //     b >>= 1;
+        // }
+
+        // return result;
+
+
+
+
+
+
+        // Another Method:
         int result = 0;
         int carry = 0;
+
         for(int k = 0; k<=31; k++){
             int ra = (a & 1);
             int rb = (b & 1);
 
-            if(carry == 0){
-                if(ra == 0 && rb == 0){
-                   
-                }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
-                    result |= (1 << k);
-                }else{
-                    carry = 1;
-                }
-            }else{
-                if(ra == 0 && rb == 0){
-                   result |= (1 << k);
-                   carry = 0;
-                }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
-                    carry = 1;
-                }else{
-                    result |= (1 << k);
-                    carry = 1;
-                }
+            int sum = ra ^ rb ^ carry;
+            carry = (ra & rb) | (ra & carry) | (rb & carry);
+
+            if(sum == 1){
+                result |= (1 << k);
             }
 
             a >>= 1;
