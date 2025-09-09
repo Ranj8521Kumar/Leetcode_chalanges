41c65b33a90f151ad50f0555f292d54293c22d9f
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-09 20:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.30%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        unsigned int ua = (unsigned int)a;
+        unsigned int ub = (unsigned int)b;
+        int result = 0;
+        int carry = 0;
+        for(int k = 0; k<=31; k++){
+            int ra = (ua & 1);
+            int rb = (ub & 1);
+
+            if(carry == 0){
+                if(ra == 0 && rb == 0){
+                   
+                }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
+                    result |= (1 << k);
+                }else{
+                    carry = 1;
+                }
+            }else{
+                if(ra == 0 && rb == 0){
+                   result |= (1 << k);
+                   carry = 0;
+                }else if((ra == 0 && rb == 1) || (ra == 1 && rb == 0)){
+                    carry = 1;
+                }else{
+                    result |= (1 << k);
+                    carry = 1;
+                }
+            }
+
+            ua >>= 1;
+            ub >>= 1;
+        }
+
+        return result;
+    }
+};
