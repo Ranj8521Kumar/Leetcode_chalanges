c94f60bc70afd03f4145a565bdfed89cf1441f96
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-10 20:57:54 +0530

    Time: 105 ms (55.73%) | Memory: 92.8 MB (94.13%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int K) {
+        int minOper = 0;
+        int Xor = 0;
+        
+        for(int k = 0; k <= 31; k++){
+            for(auto &num: nums){
+                Xor ^= ((num >> k) & 1);
+            }
+
+            if(Xor != ((K >> k) & 1)){
+                minOper++;
+            }
+
+            Xor = 0;
+        }
+
+        return minOper;
+    }
+};
