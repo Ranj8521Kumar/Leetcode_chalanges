d3802ade7c6e71b3eb5d0466b997825680bb9831
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-10 21:03:03 +0530

    Time: 131 ms (10.20%) | Memory: 93.1 MB (44.13%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int K) {
+    //     int minOper = 0;
+    //     int Xor = 0;
+        
+    //     for(int k = 0; k <= 31; k++){
+    //         for(auto &num: nums){
+    //             Xor ^= ((num >> k) & 1);
+    //         }
+
+    //         if(Xor != ((K >> k) & 1)){
+    //             minOper++;
+    //         }
+
+    //         Xor = 0;
+    //     }
+
+    //     return minOper;
+    // }
+
+
+    // Other Approach:
         int minOper = 0;
         int Xor = 0;
+
+        for(auto &num: nums){
+            Xor ^= num;
+        }
         
         for(int k = 0; k <= 31; k++){
-            for(auto &num: nums){
-                Xor ^= ((num >> k) & 1);
-            }
-
-            if(Xor != ((K >> k) & 1)){
+            if(((Xor >> k) & 1) != ((K >> k) & 1)){
                 minOper++;
             }
-
-            Xor = 0;
         }
 
         return minOper;
