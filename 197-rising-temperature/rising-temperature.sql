8338589ee9127c848fd436156e092fa72e31d1e9
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-10 23:55:52 +0530

    Time: 235 ms (60.31%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,5 +2,5 @@
 select w1.id
 from Weather w1
 join Weather w2 
-on w1.recordDate = w2.recordDate + interval '1 Day'
+on w1.recordDate = w2.recordDate + interval '1 Day' -- Date Airthmetic is used for both mysql and postgresql
 where w1.temperature > w2.temperature;
