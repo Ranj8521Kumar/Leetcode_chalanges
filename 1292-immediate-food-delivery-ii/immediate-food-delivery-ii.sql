aeabbe4763e67fa1b13c2864a243cab7eb91ec46
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-11 23:27:01 +0530

    Time: 395 ms (62.64%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -9,14 +9,14 @@
 -- order by customer_id, order_date;
 
 
-
+--- Final Query:
 select round(count(*) filter (where a.status = 'immediate')*100.00/count(a.customer_id), 2) as immediate_percentage from (
     select distinct on (customer_id)
     customer_id, 
     order_date, 
     customer_pref_delivery_date,
     case
-        when order_date = customer_pref_delivery_date then 'immediate' else 'Schedule' end as status
+        when order_date = customer_pref_delivery_date then 'immediate' else 'scheduled' end as status
 from Delivery
 order by customer_id, order_date
 ) a;
