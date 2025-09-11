b462208bdc75491bd3dc2786c2fd2fc3a63a3bb8
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-11 19:32:18 +0530

    Time: 207 ms (68.48%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-select p.product_id, round(coalesce(sum(p.price*u.units)*1.0/sum(u.units), 0), 2) as average_price from Prices  p
+select p.product_id, round(coalesce(sum(p.price*u.units)*1.0/nullif(sum(u.units), 0), 0), 2) as average_price from Prices  p
 left join UnitsSold u
 on p.product_id = u.product_id
     and purchase_date >= start_date and purchase_date <= end_date
