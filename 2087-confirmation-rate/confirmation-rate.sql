ebfb7a5e10632f8f5a8cd1df9df8065523154b9c
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-11 14:03:27 +0530

    Time: 248 ms (62.72%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -12,16 +12,26 @@
 -- group by a.user_id, b.confirmed_action;
 
 
-select aa.user_id, round(coalesce(c.confirmed_action*1.0/nullif(c.confirmation_message, 0), 0), 2) as confirmation_rate
-from Signups aa
-left join (
-    select a.user_id, count(a.user_id) as confirmation_message,
-    coalesce(b.confirmed_action, 0) as confirmed_action from Confirmations a
-    left join (
-        select user_id, count(*) as confirmed_action
-        from Confirmations
-        where action = 'confirmed'
-        group by user_id
-    ) b on a.user_id = b.user_id
-    group by a.user_id, b.confirmed_action
-) c on aa.user_id = c.user_id;
+-- select aa.user_id, round(coalesce(c.confirmed_action*1.0/nullif(c.confirmation_message, 0), 0), 2) as confirmation_rate
+-- from Signups aa
+-- left join (
+--     select a.user_id, count(a.user_id) as confirmation_message,
+--     coalesce(b.confirmed_action, 0) as confirmed_action from Confirmations a
+--     left join (
+--         select user_id, count(*) as confirmed_action
+--         from Confirmations
+--         where action = 'confirmed'
+--         group by user_id
+--     ) b on a.user_id = b.user_id
+--     group by a.user_id, b.confirmed_action
+-- ) c on aa.user_id = c.user_id;
+
+
+
+
+-- Another Simplyfy and clear Approach using filter Query:
+select s.user_id, round(coalesce(count(*) filter (where c.action = 'confirmed')*1.0 /nullif(count(c.user_id), 0), 0), 2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
