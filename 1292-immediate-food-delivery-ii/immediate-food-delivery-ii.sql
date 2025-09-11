5b6d61c4b072c332e4e6a525136e7ca1d7ae7d33
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-11 23:39:12 +0530

    Time: 603 ms (5.41%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -10,13 +10,22 @@
 
 
 --- Final Query:
-select round(count(*) filter (where a.status = 'immediate')*100.00/count(a.customer_id), 2) as immediate_percentage from (
-    select distinct on (customer_id)
-    customer_id, 
-    order_date, 
-    customer_pref_delivery_date,
-    case
-        when order_date = customer_pref_delivery_date then 'immediate' else 'scheduled' end as status
-from Delivery
-order by customer_id, order_date
-) a;
+-- select round(count(*) filter (where a.status = 'immediate')*100.00/count(a.customer_id), 2) as immediate_percentage from (
+--     select distinct on (customer_id)
+--     customer_id, 
+--     order_date, 
+--     customer_pref_delivery_date,
+--     case
+--         when order_date = customer_pref_delivery_date then 'immediate' else 'scheduled' end as status
+-- from Delivery
+-- order by customer_id, order_date
+-- ) a;
+
+--  Another Approach:
+select round(count(*) filter (where order_date = customer_pref_delivery_date)*100.00/count(*), 2) as immediate_percentage
+from Delivery d
+where order_date = (
+    select min(order_date)
+    from Delivery
+    where customer_id = d.customer_id
+);
