050db685fd586d0381309ac3de08e1943b8d004c
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-14 06:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 72.2 MB (16.59%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -10,15 +10,33 @@ public:
         return t[i] = max(nums[i] + solve(i-1, nums), nums[i]); // Either start a new or add in previous existing
     }
 
-    int maxSubArray(vector<int>& nums) {
-        n = nums.size();
-        fill(t, t+n, INT_MIN);
+    // int maxSubArray(vector<int>& nums) {
+    //     n = nums.size();
+    //     fill(t, t+n, INT_MIN);
 
-        int ans = nums[0];
-        for(int i = 0; i<n; i++){
-            ans = max(ans, solve(i, nums));
+    //     int ans = nums[0];
+    //     for(int i = 0; i<n; i++){
+    //         ans = max(ans, solve(i, nums));
+    //     }
+
+    //     return ans;
+    // }
+
+
+
+
+
+     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        int curr = nums[0];
+        int best = nums[0];
+
+        for(int i = 1; i<n; i++){
+            curr = max(nums[i], curr + nums[i]); //restart or extend
+            best = max(best, curr);
         }
 
-        return ans;
+        return best;
     }
 };
