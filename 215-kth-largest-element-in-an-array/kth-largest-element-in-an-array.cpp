9d717d392adf1afd4c4e13a18c30517f40e0428b
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-09-14 23:45:49 +0530

    Time: 27 ms (70.70%) | Memory: 61.6 MB (46.03%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>> pq(nums.begin(), nums.end());
+        // priority_queue<int, vector<int>> pq(nums.begin(), nums.end());
 
-        int cnt = 0;
-        int ans = INT_MIN;
-        while(!pq.empty()){
-            int x = pq.top();
-            pq.pop();
+        // int cnt = 0;
+        // int ans = INT_MIN;
+        // while(!pq.empty()){
+        //     int x = pq.top();
+        //     pq.pop();
 
-            cnt++;
-            if(cnt == k){
-                ans = x;
-                break;
+        //     cnt++;
+        //     if(cnt == k){
+        //         ans = x;
+        //         break;
+        //     }
+        // }
+
+        // return ans;
+
+
+
+        // Another more Optimized Approach: using mean heap that contains only k largest Number
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(auto &it: nums){
+            pq.push(it);
+
+            if(pq.size() > k){
+                pq.pop();
             }
         }
 
-        return ans;
+        return pq.top();
     }
 };
