2f1ddcf5095e989f8e6abd18966b06571a2439c9
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-01 12:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (35.19%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-    int n;
-    int t[10001];
-    bool solve(int i, vector<int> &nums){
-        if(i != n-1 && nums[i] == 0) return false;
-        if(i == n-1) return true;
+    // This is the recursion + memoization Approach:
+    // int n;
+    // int t[10001];
+    // bool solve(int i, vector<int> &nums){
+    //     if(i != n-1 && nums[i] == 0) return false;
+    //     if(i == n-1) return true;
 
-        if(t[i] != -1) return t[i];
+    //     if(t[i] != -1) return t[i];
         
-        // int j = 1;
-        // while(j<=nums[i]){
-        //     if(solve(i + j, nums) == true){
-        //         return true;
-        //     }
+    //     // int j = 1;
+    //     // while(j<=nums[i]){
+    //     //     if(solve(i + j, nums) == true){
+    //     //         return true;
+    //     //     }
 
-        //     j++;
-        // }
+    //     //     j++;
+    //     // }
 
-        for(int j = 1; j<=nums[i]; j++){
-            if(solve(i + j, nums) == true){
-                return t[i] = true;
-            }
+    //     for(int j = 1; j<=nums[i]; j++){
+    //         if(solve(i + j, nums) == true){
+    //             return t[i] = true;
+    //         }
+    //     }
+
+    //     return t[i] = false;
+    // }
+
+    // bool canJump(vector<int>& nums) {
+    //     n = nums.size();
+    //     memset(t, -1, sizeof(t));
+    //     return solve(0, nums);
+    // }
+
+
+
+
+
+
+    // Now try out using Greedy Approach:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reach = 0;
+
+        for(int i = 0; i<nums.size(); i++){
+            if(i > reach) return false;
+
+            reach = max(reach, i + nums[i]);
         }
 
-        return t[i] = false;
-    }
-
-    bool canJump(vector<int>& nums) {
-        n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(0, nums);
+        return true;
     }
 };
