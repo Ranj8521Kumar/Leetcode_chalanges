5e58a071a4440fff715f1b563217efce9fbb7da0
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-01 13:02:28 +0530

    Time: 4 ms (52.80%) | Memory: 10.9 MB (17.88%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,7 +1,43 @@
 class Solution {
 public:
+    // Recursive Approach:
+    // string makeString(string str){
+    //     string result;
+
+    //     int n = str.size();
+    //     int cnt = 1;
+    //     for(int i = 1; i<n; i++){
+    //         if(str[i] == str[i-1]){
+    //             cnt++;
+    //         }else{
+    //             result.push_back(cnt + '0');
+    //             result.push_back(str[i-1]);
+
+    //             cnt = 1;
+    //         }
+    //     }
+
+    //     result.push_back(cnt + '0');
+    //     result.push_back(str[n-1]);
+
+    //     return result;
+    // } 
+
+    // string countAndSay(int n) {
+    //     if(n == 1) return "1";
+    //     return makeString(countAndSay(n-1));
+    // }
+
+
+
+
+
+
+
+    // Iterative Approach:
+
     string makeString(string str){
-        string result;
+        string result = "";
 
         int n = str.size();
         int cnt = 1;
@@ -9,14 +45,14 @@ public:
             if(str[i] == str[i-1]){
                 cnt++;
             }else{
-                result.push_back(cnt + '0');
+                result += to_string(cnt);
                 result.push_back(str[i-1]);
 
                 cnt = 1;
             }
         }
 
-        result.push_back(cnt + '0');
+        result += to_string(cnt);
         result.push_back(str[n-1]);
 
         return result;
@@ -24,6 +60,12 @@ public:
 
     string countAndSay(int n) {
         if(n == 1) return "1";
-        return makeString(countAndSay(n-1));
+        string initialString = "1";
+
+        for(int i = 2; i<=n; i++){
+            initialString = makeString(initialString);
+        }
+
+        return initialString;
     }
 };
