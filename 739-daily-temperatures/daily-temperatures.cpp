9c8882c0789648dd5341bab4f7b0797e594f5a54
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-08 14:33:15 +0530

    Time: 31 ms (25.62%) | Memory: 102.9 MB (66.49%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -23,17 +23,40 @@ public:
 
 
         // Optimized Approach using decreasing monostack:
+        // int n = temperatures.size();
+        // vector<int> ans(n, 0);
+        // stack<int> st; //Store Indices
+
+        // for(int i = 0; i<n; i++){
+        //     while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+        //         ans[st.top()] = (i - st.top());
+        //         st.pop();
+        //     }
+
+        //     st.push(i);
+        // }
+
+        // return ans;
+
+
+        // Optimized Approach using increasing monostack:
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        stack<int> st; // Store Indices
+        stack<int> st; //Store Indices
 
-        for(int i = 0; i<n; i++){
-            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
-                ans[st.top()] = (i - st.top());
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
                 st.pop();
             }
 
-            st.push(i);
+            if(st.empty()){
+                st.push(i);
+            }
+
+            if(temperatures[i] < temperatures[st.top()]){
+                ans[i] = st.top() - i;
+                st.push(i);
+            }
         }
 
         return ans;
