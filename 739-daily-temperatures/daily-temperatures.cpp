d4b289afe6ee9c6ebd51bd30ab08c38d6aee33f0
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-08 14:26:14 +0530

    Time: 30 ms (27.85%) | Memory: 107.4 MB (38.31%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // Apply Brute Force: TLE
+        // int n = temperatures.size();
+        // vector<int> ans(n, 0);
+
+        // for(int i = 0; i<n; i++){
+        //     int cnt = 1;
+        //     for(int j = i+1; j<n; j++){
+        //         if(temperatures[j] > temperatures[i]){
+        //             ans[i] = cnt;
+        //             break;
+        //         }else{
+        //             cnt++;
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+        // Optimized Approach using decreasing monostack:
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st; // Store Indices
+
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                ans[st.top()] = (i - st.top());
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
