1a8dcd97817042f7ccb96f7e01e4dc043c4ce9b5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-11 19:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (42.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        int n = nums.size();
-
-        for(int i = 0; i<(1<<n); i++){
-            vector<int> temp;
-            int x = i;
-            for(int k=0; k<32; k++){
-                if((x & 1) == 1){
-                    temp.push_back(nums[k]);
-                }
-
-                x >>= 1;
-            }
-
+    void solve(vector<int>& nums, int i, vector<vector<int>>& result, vector<int>& temp){
+        if(i >= nums.size()){
             result.push_back(temp);
+            return;
         }
 
+        // Take current element
+        temp.push_back(nums[i]);
+        solve(nums, i+1, result, temp);
+
+        // Not Take curremt element
+        temp.pop_back();
+        solve(nums, i+1, result, temp);
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // Using Bit-Manipuation Technique:
+        // vector<vector<int>> result;
+        // int n = nums.size();
+
+        // for(int i = 0; i<(1<<n); i++){
+        //     vector<int> temp;
+        //     int x = i;
+        //     for(int k=0; k<32; k++){
+        //         if((x & 1) == 1){
+        //             temp.push_back(nums[k]);
+        //         }
+
+        //         x >>= 1;
+        //     }
+
+        //     result.push_back(temp);
+        // }
+
+        // return result;
+
+//-----------------------------------------------------//
+
+        // Using recursion:
+        int n = nums.size();
+        vector<vector<int>> result;
+        vector<int> temp;
+
+        solve(nums, 0, result, temp);
+
         return result;
+        
     }
 };
 
