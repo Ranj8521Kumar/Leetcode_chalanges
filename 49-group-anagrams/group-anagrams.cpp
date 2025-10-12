e6a76ed1daa374fa65bc539356db39ba06eec693
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-12 21:21:23 +0530

    Time: 17 ms (56.00%) | Memory: 25 MB (66.25%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+
+        for(auto &str: strs){
+            string temp = str;
+            sort(temp.begin(), temp.end());
+            mpp[temp].push_back(str);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto &it: mpp){
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
