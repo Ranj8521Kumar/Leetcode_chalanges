988699f0079848d592f7a50cf21c7054c0c3c099
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-13 02:04:27 +0530

    Time: 19 ms (25.97%) | Memory: 26.6 MB (5.08%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> temp(n, 0);
+        // vector<int> temp(n, 0);
 
+        // for(int i = 1; i<=n; i++){
+        //     temp[i-1] = i;
+        // }
+
+        // int start = 0;
+        // while(temp.size() != 1){
+        //     int del = (start + k - 1) % temp.size();
+        //     temp.erase(temp.begin() + del);
+
+        //     start = del;
+        // }
+
+        // return temp[0];
+
+
+
+        // ------------------------------------------ //
+
+        // Using Queue:
+        queue<int> que;
         for(int i = 1; i<=n; i++){
-            temp[i-1] = i;
+            que.push(i);
         }
 
-        int start = 0;
-        while(temp.size() != 1){
-            int del = (start + k - 1) % temp.size();
-            temp.erase(temp.begin() + del);
+        while(que.size() != 1){
+            for(int i = 1; i<k; i++){
+                int x = que.front();
+                que.pop();
 
-            start = del;
+                que.push(x);
+            }
+
+            que.pop();
         }
 
-        return temp[0];
+        return que.front();
     }
 };
