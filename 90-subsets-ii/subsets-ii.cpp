b9ae197a4226d0ee1c3787530115f7a295732177
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-12 19:47:54 +0530

    Time: 4 ms (17.11%) | Memory: 10.5 MB (66.47%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -35,37 +35,70 @@
 // ----------------------------------------------- //
 
 //Without using set
+// class Solution {
+// public:
+//     void solve(vector<int>& nums, int i, vector<vector<int>> &result, vector<int>& temp){
+//         if(i >= nums.size()){
+//             result.push_back(temp);
+//             return;
+//         }
+
+//         //take current element
+//         temp.push_back(nums[i]);
+//         solve(nums, i+1, result, temp);
+
+//         //backtrack
+//         temp.pop_back();
+
+
+//         // Skip dublicates:
+//         while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+
+//         // Not take current element
+//         solve(nums, i+1, result, temp);
+//     }
+
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         // Using backtracking:
+//         int n = nums.size();
+//         vector<vector<int>> result;
+//         vector<int> temp;
+//         sort(nums.begin(), nums.end());
+
+//         solve(nums, 0, result, temp);
+
+//         return result;
+//     }
+// };
+
+
+// ---------------------------------------------------- //
+
+// Using Bit manipulation techniques:
 class Solution {
 public:
-    void solve(vector<int>& nums, int i, vector<vector<int>> &result, vector<int>& temp){
-        if(i >= nums.size()){
-            result.push_back(temp);
-            return;
-        }
-
-        //take current element
-        temp.push_back(nums[i]);
-        solve(nums, i+1, result, temp);
-
-        //backtrack
-        temp.pop_back();
-
-
-        // Skip dublicates:
-        while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
-
-        // Not take current element
-        solve(nums, i+1, result, temp);
-    }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // Using backtracking:
         int n = nums.size();
         vector<vector<int>> result;
         vector<int> temp;
         sort(nums.begin(), nums.end());
 
-        solve(nums, 0, result, temp);
+        for(int i = 0; i<=((1<<n) - 1); i++){
+            int x = i;
+            for(int k = 0; k<n; k++){
+                if((x&1) == 1){
+                    temp.push_back(nums[k]);
+                }
+
+                x >>= 1;
+            }
+
+            result.push_back(temp);
+            temp.clear();
+        }
+
+        sort(result.begin(), result.end());
+        result.erase(unique(result.begin(), result.end()), result.end());
 
         return result;
     }
