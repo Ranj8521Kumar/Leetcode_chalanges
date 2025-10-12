58961ea27974ece48e8dc696a2468f84c064e6a2
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-12 19:22:19 +0530

    Time: 3 ms (34.70%) | Memory: 10.5 MB (48.90%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,71 @@
+// class Solution {
+// public:
+//     void solve(vector<int>& nums, int i, set<vector<int>> &st, vector<int>& temp){
+//         if(i >= nums.size()){
+//             st.insert(temp);
+//             return;
+//         }
+
+//         //take current element
+//         temp.push_back(nums[i]);
+//         solve(nums, i+1, st, temp);
+
+//         //backtrack
+//         temp.pop_back();
+//         solve(nums, i+1, st, temp);
+//     }
+
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         // Using backtracking:
+//         int n = nums.size();
+//         vector<int> temp;
+//         set<vector<int>> st;
+//         sort(nums.begin(), nums.end());
+
+//         solve(nums, 0, st, temp);
+
+//         vector<vector<int>> result(st.begin(), st.end());
+
+//         return result;
+//     }
+// };
+
+
+
+// ----------------------------------------------- //
+
+//Without using set
 class Solution {
 public:
-    void solve(vector<int>& nums, int i, set<vector<int>> &st, vector<int>& temp){
+    void solve(vector<int>& nums, int i, vector<vector<int>> &result, vector<int>& temp){
         if(i >= nums.size()){
-            st.insert(temp);
+            result.push_back(temp);
             return;
         }
 
         //take current element
         temp.push_back(nums[i]);
-        solve(nums, i+1, st, temp);
+        solve(nums, i+1, result, temp);
 
         //backtrack
         temp.pop_back();
-        solve(nums, i+1, st, temp);
+
+
+        // Skip dublicates:
+        while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+
+        // Not take current element
+        solve(nums, i+1, result, temp);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         // Using backtracking:
         int n = nums.size();
+        vector<vector<int>> result;
         vector<int> temp;
-        set<vector<int>> st;
         sort(nums.begin(), nums.end());
 
-        solve(nums, 0, st, temp);
-
-        vector<vector<int>> result(st.begin(), st.end());
+        solve(nums, 0, result, temp);
 
         return result;
     }
