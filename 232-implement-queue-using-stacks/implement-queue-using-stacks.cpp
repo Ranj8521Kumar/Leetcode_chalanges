56d82a71b3aaa52392d6e830e6e65636b29e0351
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-13 03:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (99.58%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,29 +1,87 @@
+// class MyQueue {
+// public:
+//     stack<int> s1,s2;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) {
+//         while(!s1.empty()){
+//             int x = s1.top();
+//             s1.pop();
+//             s2.push(x);
+//         }
+
+//         s2.push(x);
+//         while(!s2.empty()){
+//             int x = s2.top();
+//             s2.pop();
+//             s1.push(x);
+//         }
+//     }
+    
+//     int pop() {
+//         if(!s1.empty()){
+//             int x = s1.top();
+//             s1.pop();
+
+//             return x;
+//         }
+
+//         return -1;
+//     }
+    
+//     int peek() {
+//         return s1.top();
+//     }
+    
+//     bool empty() {
+//         if(!s1.empty()){
+//             return false;
+//         }
+
+
+//         return true;
+//     }
+// };
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue* obj = new MyQueue();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->peek();
+//  * bool param_4 = obj->empty();
+//  */
+
+
+
+// --------------------------------------------------- //
+// Using one stack:
+
 class MyQueue {
 public:
-    stack<int> s1,s2;
+    stack<int> s;
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            int x = s1.top();
-            s1.pop();
-            s2.push(x);
+        if(s.empty()){
+            s.push(x);
+            return;
         }
 
-        s2.push(x);
-        while(!s2.empty()){
-            int x = s2.top();
-            s2.pop();
-            s1.push(x);
-        }
+        int topElem = s.top();
+        s.pop();
+        push(x);
+        s.push(topElem);
     }
     
     int pop() {
-        if(!s1.empty()){
-            int x = s1.top();
-            s1.pop();
+        if(!s.empty()){
+            int x = s.top();
+            s.pop();
 
             return x;
         }
@@ -32,11 +90,11 @@ public:
     }
     
     int peek() {
-        return s1.top();
+        return s.top();
     }
     
     bool empty() {
-        if(!s1.empty()){
+        if(!s.empty()){
             return false;
         }
 
