132c20730c4550eb777dd01158df729530982549
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-13 03:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (86.24%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,28 +1,85 @@
+// class MyStack {
+// public:
+//     queue<int> q1;
+//     queue<int> q2;
+
+//     MyStack() {
+        
+//     }
+    
+//     void push(int x) {
+//         q2.push(x);
+
+//         while(!q1.empty()){
+//             int x = q1.front();
+//             q2.push(x);
+//             q1.pop();
+//         }
+
+//         swap(q1, q2);
+//     }
+    
+//     int pop() {
+//         if(!q1.empty()){
+//             int x = q1.front();
+//             q1.pop();
+//             return x;
+//         }
+
+//         return -1;
+//     }
+    
+//     int top() {
+//         return q1.front();
+//     }
+    
+//     bool empty() {
+//         if(q1.empty()){
+//             return true;
+//         }
+
+//         return false;
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
+
+
+
+// ------------------------------------------------- //
+// Using One queue:
+
 class MyStack {
 public:
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
 
     MyStack() {
         
     }
     
     void push(int x) {
-        q2.push(x);
+        int len =  q.size();
 
-        while(!q1.empty()){
-            int x = q1.front();
-            q2.push(x);
-            q1.pop();
+        q.push(x);
+        while(len--){
+            int x = q.front();
+            q.pop();
+
+            q.push(x);
         }
-
-        swap(q1, q2);
     }
     
     int pop() {
-        if(!q1.empty()){
-            int x = q1.front();
-            q1.pop();
+        if(!q.empty()){
+            int x = q.front();
+            q.pop();
             return x;
         }
 
@@ -30,11 +87,11 @@ public:
     }
     
     int top() {
-        return q1.front();
+        return q.front();
     }
     
     bool empty() {
-        if(q1.empty()){
+        if(q.empty()){
             return true;
         }
 
