8e6274f213736cd58c60f3b3656b0336b0b35285
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-14 11:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (24.56%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -8,8 +8,56 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         // Find Middle
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+//         while(fast && fast->next){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+
+//         stack<ListNode*> st;
+//         ListNode* tempSlow = slow;
+//         while(slow->next){
+//             st.push(slow->next);
+//             slow = slow->next;
+//         }
+
+//         tempSlow->next = nullptr;
+
+//         ListNode *temp = head;
+//         while(!st.empty()){
+//             ListNode *node = st.top(); st.pop();
+//             ListNode *t = temp->next;
+//             temp->next = node;
+//             node->next = t;
+
+//             temp = temp->next->next;
+//         }
+//     }
+// };
+
+
+
+// -------------------------------------------------------- //
+
 class Solution {
 public:
+    ListNode* reverse(ListNode* head){
+        if(!head || !head->next) return head;
+
+        ListNode* newHead = reverse(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newHead;
+    }
+
     void reorderList(ListNode* head) {
         // Find Middle
         ListNode* slow = head;
@@ -20,21 +68,15 @@ public:
             fast = fast->next->next;
         }
 
-        stack<ListNode*> st;
         ListNode* tempSlow = slow;
-        while(slow->next){
-            st.push(slow->next);
-            slow = slow->next;
-        }
-
-        tempSlow->next = nullptr;
+        tempSlow->next = reverse(slow->next);
 
         ListNode *temp = head;
-        while(!st.empty()){
-            ListNode *node = st.top(); st.pop();
-            ListNode *t = temp->next;
-            temp->next = node;
-            node->next = t;
+        while(tempSlow->next){
+            ListNode *node = temp->next;
+            temp->next = tempSlow->next;
+            tempSlow->next = tempSlow->next->next;
+            temp->next->next = node;
 
             temp = temp->next->next;
         }
