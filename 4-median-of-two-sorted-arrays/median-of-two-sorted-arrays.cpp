2ee3a8e7f3e7db8857e7f002d10c0e6e96c6f6d5
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-14 15:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (80.69%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -33,61 +33,111 @@
 
 
 
+// // ------------------------------------------------------ //
+// // Most Efficient Approach -> using Binary Search
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         double result = 0.0;
+
+//         if(m > n){
+//             vector<int> temp = nums1;
+//             nums1 = nums2;
+//             nums2 = temp;
+//             swap(m, n);
+//         }
+
+//         bool oddLength = false;
+//         if((m + n) % 2 != 0){
+//             oddLength = true;
+//         }
+
+//         int len; //firstHalfLegth
+//         if(oddLength){
+//             len = (m + n + 1) / 2;
+//         }else{
+//             len = (m + n) / 2;
+//         }
+
+//         int low = 0;
+//         int high = m;
+
+//         while(low <= high){
+//             int px = low + (high - low) / 2;
+
+//             int x1 = (px)?nums1[px-1]:INT_MIN;
+//             int x3 = (px == m)?INT_MAX:nums1[px];
+
+//             int x2 = ((len-px) == 0)?INT_MIN:nums2[len-px-1];
+//             int x4 = ((len-n) == 0 && px == 0)?INT_MAX:nums2[len-px];
+
+//             if(x1 > x4){
+//                 high = px - 1;
+//             }else if(x2 > x3){
+//                 low = px + 1;
+//             }
+
+//             if(x1 <= x4 && x2 <= x3){
+//                 if(oddLength){
+//                     result = (max(x1, x2));
+//                 }else{
+//                     result = (double)(max(x1, x2) + min(x3, x4)) / 2;
+//                 }
+
+//                 break;
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+
+
+
+
 // ------------------------------------------------------ //
-// Most Efficient Approach -> using Binary Search
+// Most Efficient Approach -> using Binary Search -> remove unused code
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
         int m = nums1.size();
         int n = nums2.size();
         double result = 0.0;
-
-        if(m > n){
-            vector<int> temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
-            swap(m, n);
-        }
-
-        bool oddLength = false;
-        if((m + n) % 2 != 0){
-            oddLength = true;
-        }
-
-        int len; //firstHalfLegth
-        if(oddLength){
-            len = (m + n + 1) / 2;
-        }else{
-            len = (m + n) / 2;
-        }
 
         int low = 0;
         int high = m;
 
         while(low <= high){
             int px = low + (high - low) / 2;
+            int py = ((m + n + 1) / 2) - px;
 
             int x1 = (px)?nums1[px-1]:INT_MIN;
             int x3 = (px == m)?INT_MAX:nums1[px];
 
-            int x2 = ((len-px) == 0)?INT_MIN:nums2[len-px-1];
-            int x4 = ((len-n) == 0 && px == 0)?INT_MAX:nums2[len-px];
-
-            if(x1 > x4){
-                high = px - 1;
-            }else if(x2 > x3){
-                low = px + 1;
-            }
+            int x2 = (py == 0)?INT_MIN:nums2[py - 1];
+            int x4 = ((py == n))?INT_MAX:nums2[py];
 
             if(x1 <= x4 && x2 <= x3){
-                if(oddLength){
+                if((m+n)%2 != 0){
                     result = (max(x1, x2));
                 }else{
                     result = (double)(max(x1, x2) + min(x3, x4)) / 2;
                 }
-
                 break;
+            }
+
+            if(x1 > x4){
+                high = px - 1;
+            }else{
+                low = px + 1;
             }
         }
 
