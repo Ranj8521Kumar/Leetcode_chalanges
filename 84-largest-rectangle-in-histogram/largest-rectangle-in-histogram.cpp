d894fe95c5b5f09a31187afb09fc5006ecb00932
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-15 02:37:28 +0530

    Time: 3 ms (98.83%) | Memory: 81.5 MB (65.72%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,57 +1,90 @@
 class Solution {
 public:
-    vector<int> nextSmallerElem(vector<int>& nums, int n){
-        vector<int> result(n, -1);
-        stack<int> st; // store Indices
+    // vector<int> nextSmallerElem(vector<int>& nums, int n){
+    //     vector<int> result(n, -1);
+    //     stack<int> st; // store Indices
 
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && nums[i] <= nums[st.top()]){
-                st.pop();
-            }
+    //     for(int i=n-1; i>=0; i--){
+    //         while(!st.empty() && nums[i] <= nums[st.top()]){
+    //             st.pop();
+    //         }
 
-            if(st.empty()){
-                result[i] = n;
-            }else{
-                result[i] = st.top();
-            }
+    //         if(st.empty()){
+    //             result[i] = n;
+    //         }else{
+    //             result[i] = st.top();
+    //         }
 
-            st.push(i);
-        }
+    //         st.push(i);
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    vector<int> prevSmallerElem(vector<int>& nums, int n){
-        vector<int> result(n, -1);
-        stack<int> st; // store Indices
+    // vector<int> prevSmallerElem(vector<int>& nums, int n){
+    //     vector<int> result(n, -1);
+    //     stack<int> st; // store Indices
 
-        for(int i=0; i<n; i++){
-            while(!st.empty() && nums[i] <= nums[st.top()]){
-                st.pop();
-            }
+    //     for(int i=0; i<n; i++){
+    //         while(!st.empty() && nums[i] <= nums[st.top()]){
+    //             st.pop();
+    //         }
 
-            if(st.empty()){
-                result[i] = -1;
-            }else{
-                result[i] = st.top();
-            }
+    //         if(st.empty()){
+    //             result[i] = -1;
+    //         }else{
+    //             result[i] = st.top();
+    //         }
 
-            st.push(i);
-        }
+    //         st.push(i);
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
-    int largestRectangleArea(vector<int>& heights) {
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+//         vector<int> NSE = nextSmallerElem(heights, n); // Store the indices
+//         vector<int> PSE = prevSmallerElem(heights, n); // store the indices
+
+//         int maxArea = 0;
+//         for(int i = 0; i<n; i++){
+//             int height = heights[i];
+//             int width = (NSE[i] - PSE[i]) - 1;
+//             maxArea = max(maxArea, height*width);
+//         }
+
+//         return maxArea;
+//     }
+// };
+
+
+
+// ------------------------------------------------------ //
+// Optimized Approach:
+int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> NSE = nextSmallerElem(heights, n); // Store the indices
-        vector<int> PSE = prevSmallerElem(heights, n); // store the indices
+        stack<int> st;
 
         int maxArea = 0;
         for(int i = 0; i<n; i++){
-            int height = heights[i];
-            int width = (NSE[i] - PSE[i]) - 1;
-            maxArea = max(maxArea, height*width);
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                int nse = i;
+                int element =  st.top(); st.pop();
+                int pse = (st.empty())?-1:st.top();
+
+                maxArea = max(maxArea, heights[element] * (nse - pse - 1));
+            }
+
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int nse = n;
+            int element = st.top(); st.pop();
+
+            int pse = (st.empty())?-1:st.top();
+            maxArea = max(maxArea, heights[element] * (nse - pse - 1));
         }
 
         return maxArea;
