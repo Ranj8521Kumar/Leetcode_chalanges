f0e7db53eee82b81f8aa9fa9a1d418066f8c69ee
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-17 01:55:07 +0530

    Time: 3 ms (20.06%) | Memory: 60.9 MB (30.08%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -9,36 +9,74 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// class Solution {
+// public:
+//     void inOrder(TreeNode* root, vector<TreeNode*>& nodes){
+//         if(!root) return;
+
+//         inOrder(root->left, nodes);
+//         nodes.push_back(root);
+//         inOrder(root->right, nodes);
+//     }
+
+//     void recoverTree(TreeNode* root) {
+//         if(!root) return;
+
+//         vector<TreeNode*> nodes;
+
+//         inOrder(root, nodes);
+
+//         int n = nodes.size();
+//         TreeNode* firstNode = nullptr;
+//         TreeNode* secondNode = nullptr;
+
+//         for(int i = 0; i<n-1; i++){
+//             if(nodes[i]->val > nodes[i+1]->val){
+//                 if(!firstNode){
+//                     firstNode = nodes[i];// Store the first misplaced Node
+//                 }
+
+//                 secondNode = nodes[i+1];// Store the last misplaced Node
+//             }
+//         }
+
+//         if(firstNode && secondNode){
+//             swap(firstNode->val, secondNode->val);
+//         }
+//     }
+// };
+
+
+
+
+// ---------------------------------------------------- //
+// Second Approach, without using the extra space
 class Solution {
 public:
-    void inOrder(TreeNode* root, vector<TreeNode*>& nodes){
+    TreeNode* firstNode = nullptr;
+    TreeNode* secondNode = nullptr;
+    TreeNode* prevNode = new TreeNode(INT_MIN);
+
+    void inOrder(TreeNode* root){
         if(!root) return;
 
-        inOrder(root->left, nodes);
-        nodes.push_back(root);
-        inOrder(root->right, nodes);
+        inOrder(root->left);
+        if(prevNode && prevNode->val > root->val){
+            if(!firstNode){
+                firstNode = prevNode;
+            }
+            secondNode = root;
+        }
+        prevNode = root;
+
+        inOrder(root->right);
     }
 
     void recoverTree(TreeNode* root) {
         if(!root) return;
-
-        vector<TreeNode*> nodes;
-
-        inOrder(root, nodes);
-
-        int n = nodes.size();
-        TreeNode* firstNode = nullptr;
-        TreeNode* secondNode = nullptr;
-
-        for(int i = 0; i<n-1; i++){
-            if(nodes[i]->val > nodes[i+1]->val){
-                if(!firstNode){
-                    firstNode = nodes[i];// Store the first misplaced Node
-                }
-
-                secondNode = nodes[i+1];// Store the last misplaced Node
-            }
-        }
+        inOrder(root);
 
         if(firstNode && secondNode){
             swap(firstNode->val, secondNode->val);
