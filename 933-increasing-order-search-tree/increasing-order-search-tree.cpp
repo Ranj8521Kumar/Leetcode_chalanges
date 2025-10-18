b5ee00bf39384596eb066efdba798fc6f47ce0be
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-18 13:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (74.79%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.cpp
+++ 933-increasing-order-search-tree/increasing-order-search-tree.cpp
@@ -9,35 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// class Solution {
+// public:
+//     void fillStack(TreeNode* root, stack<TreeNode*>& st){
+//         if(!root) return;
+        
+//         fillStack(root->right, st);
+//         st.push(root);
+//         fillStack(root->left, st);
+//     }
+
+//     TreeNode* increasingBST(TreeNode* root) {
+//         stack<TreeNode*> st;
+
+//         fillStack(root, st);
+
+//         TreeNode* newRoot = st.top();
+//         TreeNode* temp = newRoot;
+//         st.pop();
+
+//         while(!st.empty()){
+//             TreeNode* node = st.top(); st.pop();
+//             temp->left = nullptr;
+//             temp->right = node;
+
+//             temp = temp->right;
+//         }
+
+//         temp->left = nullptr;
+//         temp->right = nullptr;
+//         return newRoot;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void fillStack(TreeNode* root, stack<TreeNode*>& st){
-        if(!root) return;
-        
-        fillStack(root->right, st);
-        st.push(root);
-        fillStack(root->left, st);
-    }
-
     TreeNode* increasingBST(TreeNode* root) {
-        stack<TreeNode*> st;
+        TreeNode* dummy = new TreeNode(-1);
+        TreeNode* prev = dummy;
+        TreeNode* curr = root;
 
-        fillStack(root, st);
+        while (curr) {
+            if (curr->left == nullptr) {
+                prev->right = curr;
+                prev = curr;
+                curr = curr->right;
+            } else {
+                TreeNode* predecessor = curr->left;
+                while (predecessor->right != nullptr) {
+                    predecessor = predecessor->right;
+                }
 
-        TreeNode* newRoot = st.top();
-        TreeNode* temp = newRoot;
-        st.pop();
-
-        while(!st.empty()){
-            TreeNode* node = st.top(); st.pop();
-            temp->left = nullptr;
-            temp->right = node;
-
-            temp = temp->right;
+                predecessor->right = curr;
+                TreeNode* temp = curr->left;
+                curr->left = nullptr;
+                curr = temp;
+            }
         }
 
-        temp->left = nullptr;
-        temp->right = nullptr;
-        return newRoot;
+        return dummy->right;
     }
 };
