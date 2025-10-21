98361c8f4cd4a68bc0b53cef46b1da8a226e18d0
Ranjan Kumar Pandit <22it3037@rgipt.ac.in>
2025-10-22 01:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 30.7 MB (100.00%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -9,6 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// class Solution {
+// public:
+//     vector<TreeNode*> solve(int n){
+//         if(n%2 == 0) return {};
+
+//         if(n == 1){
+//             TreeNode* root = new TreeNode(0);
+//             return {root};
+//         }
+
+//         vector<TreeNode*> result;
+
+//         for(int i = 1; i<n; i+=2){
+//             vector<TreeNode*> Left = solve(i);
+//             vector<TreeNode*> Right = solve(n - i - 1);
+
+//             for(auto &lft: Left){
+//                 for(auto &rht: Right){
+//                     TreeNode* root = new TreeNode(0);
+//                     root->left = lft;
+//                     root->right = rht;
+
+//                     result.push_back(root);
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+
+//     vector<TreeNode*> allPossibleFBT(int n) {
+//         return solve(n);
+//     }
+// };
+
+
+
+// ---------------------------------------------------------- //
+// Let's Memoize this:
+unordered_map<int, vector<TreeNode*>> mpp;
 class Solution {
 public:
     vector<TreeNode*> solve(int n){
@@ -19,11 +60,15 @@ public:
             return {root};
         }
 
+        if(mpp.find(n) != mpp.end()) return mpp[n]; 
+
         vector<TreeNode*> result;
 
         for(int i = 1; i<n; i+=2){
             vector<TreeNode*> Left = solve(i);
+            mpp[i] = Left;
             vector<TreeNode*> Right = solve(n - i - 1);
+            mpp[n-i-1] = Right;
 
             for(auto &lft: Left){
                 for(auto &rht: Right){
